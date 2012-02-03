469869e2f0b928a36a5c71778a2d17d013f2e4f9
Daniel Naber <naber@danielnaber.de>
2012-02-03 20:39:52 +0000

    [web] update the screenshot page

--- www/screenshots/index.php
+++ www/screenshots/index.php
@@ -2,49 +2,42 @@
 $page = "screenshots";
 $title = "LanguageTool";
 $title2 = "Screenshots";
-$lastmod = "2008-08-07 22:00:00 CET";
+$lastmod = "2012-02-03 22:00:00 CET";
 include("../../include/header.php");
 ?>
 		
 <table border="0" cellpadding="0" cellspacing="0" style="margin-left:5px;">
 <tr><td>&nbsp;</td></tr>
 <tr>
-<td>
-	<a href="art/screenshot_ooo3_pl.png"><img border="0" width="320" height="235" 
-		src="art/screenshot_ooo3_pl_small.png" alt="LanguageTool in Polish OpenOffice.org 3.0" /></a><br />
-		<a href="art/screenshot_ooo3_pl.png"><span style="font-size:10px">LanguageTool installed
-		as an add-on<br/>in Polish OpenOffice.org 3.0.1 (click to enlarge)</span></a>
-</td>
+    <td valign="top" align="middle">
+        <a href="art/screenshot_lo3.png"><img border="0" width="320" height="235"
+            src="art/screenshot_lo3_small.png" alt="LanguageTool in LibreOffice 3.3" /></a><br />
+            <a href="art/screenshot_lo3.png"><span style="font-size:10px">LanguageTool installed
+            as an add-on<br/>in LibreOffice 3.3</span></a>
+    </td>
+    <td style="width:20px">&nbsp;&nbsp;</td>
+    <td valign="top" align="middle">
+        <a href="art/screenshot_ooo3_pl.png"><img border="0" width="320" height="235"
+            src="art/screenshot_ooo3_pl_small.png" alt="LanguageTool in Polish OpenOffice.org 3.0" /></a><br />
+            <a href="art/screenshot_ooo3_pl.png"><span style="font-size:10px">LanguageTool installed
+            as an add-on<br/>in Polish OpenOffice.org 3.0.1</span></a>
+    </td>
 </tr>
 <tr><td>&nbsp;</td></tr>
-<tr><td>&nbsp;</td></tr>
 <tr>
-<td>
-	<a href="art/screenshot_ooo3.png"><img border="0" width="300" height="216" 
-		src="art/screenshot_ooo3_small.png" alt="LanguageTool in OpenOffice.org 3.0" /></a><br />
-		<a href="art/screenshot_ooo3.png"><span style="font-size:10px">LanguageTool installed
-		as an add-on<br/>in OpenOffice.org 3.0 (click to enlarge)</span></a>
-</td>
-</tr>
-<tr><td>&nbsp;</td></tr>
-<tr><td>&nbsp;</td></tr>
-<tr>
-<td>
-	<a href="art/screenshot.png"><img border="0" width="250" height="257"
-		src="art/screenshot_small.png" alt="stand-alone version of LanguageTool" /></a><br />
-		<a href="art/screenshot.png"><span style="font-size:10px">The simple stand-alone
-		user interface which lets<br/>you type in or paste text and check it (click to enlarge)</span></a>
-</td>
-</tr>
-<tr><td>&nbsp;</td></tr>
-<tr><td>&nbsp;</td></tr>
-<tr>
-<td>
-	<a href="art/lt-omegat.png"><img border="0" width="300" height="175" 
-		src="art/lt-omegat_small.png" alt="LanguageTool plugin in OmegaT" /></a><br />
-		<a href="art/lt-omegat.png"><span style="font-size:10px">The simple LanguageTool
-		plugin in OmegaT<br/>that helps to check your translation on the go(click to enlarge)</span></a>
-</td>
+    <td valign="top" align="middle">
+        <a href="art/screenshot.png"><img border="0" width="320" height="235"
+            src="art/screenshot_small.png" alt="stand-alone version of LanguageTool" /></a><br />
+            <a href="art/screenshot.png"><span style="font-size:10px">The simple stand-alone
+            user interface which lets<br/>you type in or paste text and check it</span></a>
+    </td>
+    <td></td>
+    <td valign="top" align="middle">
+        <a href="art/lt-omegat.png"><img border="0" width="320" height="186"
+            src="art/lt-omegat_small.png" alt="LanguageTool plugin in OmegaT" /></a><br />
+            <a href="art/lt-omegat.png"><span style="font-size:10px">The simple LanguageTool
+            plugin in OmegaT<br/>that helps to check your translation on the go</span></a>
+    </td>
 </tr>
 
 </table>
