b59c2d2736ec974701a6f3172a9db7d40c61b01f
Markus Brenneis <support.gulp21@googlemail.com>
2012-02-09 06:45:13 +0000

    [web] add a php template for download button

---
+++ include/download.php
@@ -0,0 +1,14 @@
+<?php
+$version= "1.6";
+$filesize = "29";
+if( !isset($downloadPath) $downloadPath = "download" )
+if( !isset($downloadLabel) $downloadLabel = "Download" )
+?>
+
+<div id="downloadButton">
+   <?php print "<a style=\"display: block\" href=\"$downloadPath/LanguageTool-$version.oxt\">"; ?>
+     <span class="languagetool">LanguageTool</span><br/>
+     <?php print "<span class=\"download\">$downloadLabel<br/></span>"; ?>
+     <?php print "<span class=\"version\">Version $version ($filesize&nbsp;MB)</span>"; ?>
+   </a>
+</div> 
