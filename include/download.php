0eda2694836dec037bafd975eb2bad9b36fd79d4
Daniel Naber <naber@danielnaber.de>
2012-03-24 23:31:35 +0000

    link LanguageTool-stable.oxt, but also add version number to prevent caching the old version

--- include/download.php
+++ include/download.php
@@ -6,7 +6,7 @@ if( !isset($downloadLabel) ) $downloadLabel = "Download";
 ?>
 
 <div id="downloadButton">
-   <a style="display: block" href="<?php print $downloadPath ?>/LanguageTool-<?php print $version ?>.oxt">
+   <a style="display: block" href="<?php print $downloadPath ?>/LanguageTool-stable.oxt?<?php print $version ?>">
      <span class="languagetool">LanguageTool</span><br/>
      <span class="download"><?php print $downloadLabel ?><br/></span>
      <span class="version">Version <?php print $version ?> (<?php print $filesize ?>&nbsp;MB)</span>
