f0d9bbca897d8400e9c950674f5d6505e6f03117
Daniel Naber <naber@danielnaber.de>
2012-08-04 21:38:21 +0000

    make snapshots also for stand-alone version

--- create-snapshot.sh
+++ create-snapshot.sh
@@ -4,8 +4,13 @@
 cd /home/languagetool/languagetool.org
 svn up svn-checkout
 cd svn-checkout
+
 ant dist
 mv dist/LanguageTool-*-dev.oxt ../www/download/snapshots/LanguageTool-`date +%Y%m%d`-snapshot.oxt
 
+ant dist-standalone
+mv dist/LanguageTool-*-dev.zip ../www/download/snapshots/LanguageTool-`date +%Y%m%d`-snapshot.zip
+
 # delete *.oxt files older than 5 days:
 rm `find ../www/download/snapshots/ -name "*.oxt" -mtime +5`
+rm `find ../www/download/snapshots/ -name "*.zip" -mtime +5`
