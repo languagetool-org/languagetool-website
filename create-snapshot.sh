1f76dbd3e3ab69ae884a45643c4e913cb06cc238
Daniel Naber <naber@danielnaber.de>
2012-08-23 21:09:07 +0000

    adapt to new server

--- create-snapshot.sh
+++ create-snapshot.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # compiles the current LanguageTool code and moves the result to a snapshots directory
 
+SNAPSHOT_DIR=../website-from-svn/www/download/snapshots
+
 cd /home/languagetool/languagetool.org
 svn up svn-checkout
 cd svn-checkout
 
-ant dist
-mv dist/LanguageTool-*-dev.oxt ../www/download/snapshots/LanguageTool-`date +%Y%m%d`-snapshot.oxt
+ant dist && mv dist/LanguageTool-*-dev.oxt $SNAPSHOT_DIR/LanguageTool-`date +%Y%m%d`-snapshot.oxt
 
-ant dist-standalone
-mv dist/LanguageTool-*-dev.zip ../www/download/snapshots/LanguageTool-`date +%Y%m%d`-snapshot.zip
+ant dist-standalone && mv dist/LanguageTool-*-dev.zip $SNAPSHOT_DIR/LanguageTool-`date +%Y%m%d`-snapshot.zip
 
 # delete *.oxt files older than 5 days:
-rm `find ../www/download/snapshots/ -name "*.oxt" -mtime +5`
-rm `find ../www/download/snapshots/ -name "*.zip" -mtime +5`
+rm `find $SNAPSHOT_DIR -name "*.oxt" -mtime +5`
+rm `find $SNAPSHOT_DIR -name "*.zip" -mtime +5`
