14eaa07163c1f684877cdcbba6642979434121ea
Daniel Naber <naber@danielnaber.de>
2012-10-01 08:58:34 +0000

    link the files with the version number, not the "-stable" ones: this is less confusing for the user, as the version number is then in the local file name

--- include/download.php
+++ include/download.php
@@ -8,7 +8,7 @@ if( !isset($downloadPath) )         $downloadPath = "download";
 ?>
 
 <div class="downloadButton">
-   <a class='piwik_download' style="display: block" href="<?php print $downloadPath ?>/LanguageTool-stable.oxt?<?php print $version ?>">
+   <a class='piwik_download' style="display: block" href="<?php print $downloadPath ?>/LanguageTool-<?php print $version ?>.oxt">
      <span class="languagetool"><?php print $downloadTitle ?></span><br/>
      <span class="download"><?php print $downloadLabel ?><br/></span>
      <span class="version"><?php print $downloadVersionLabel ?> <?php print $version ?> (<?php print $filesize ?>&nbsp;MB)</span>
--- include/downloadStandAlone.php
+++ include/downloadStandAlone.php
@@ -1,6 +1,6 @@
 <?php
 $version= "1.9";
-$filesize = "57";
+$filesize = "57";   //MB
 if( !isset($downloadTitleStandAlone) )        $downloadTitleStandAlone = "Download LanguageTool";
 if( !isset($downloadLabelStandAlone) )        $downloadLabelStandAlone = "for stand-alone use";
 if( !isset($downloadVersionLabelStandAlone) ) $downloadVersionLabel = "Version";
@@ -11,7 +11,7 @@ if( !isset($downloadPathStandAlone) )         $downloadPathStandAlone = "downloa
   <tr>
     <td>
       <div class="downloadButton">
-         <a class='piwik_download' style="display: block" href="<?php print $downloadPathStandAlone ?>/LanguageTool-stable.zip?<?php print $version ?>">
+         <a class='piwik_download' style="display: block" href="<?php print $downloadPathStandAlone ?>/LanguageTool-<?php print $version ?>.zip">
            <span class="languagetool"><?php print $downloadTitleStandAlone ?></span><br/>
            <span class="download"><?php print $downloadLabelStandAlone ?><br/></span>
            <span class="version"><?php print $downloadVersionLabel ?> <?php print $version ?> (<?php print $filesize ?>&nbsp;MB)</span>
