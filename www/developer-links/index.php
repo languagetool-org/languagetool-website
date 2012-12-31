348deb259ddd411a078cd93980d70a07a81bb23f
Daniel Naber <naber@danielnaber.de>
2012-12-31 11:28:58 +0000

    add links to interesting libraries etc; whitespace cleanup

--- www/developer-links/index.php
+++ www/developer-links/index.php
@@ -2,7 +2,7 @@
 $page = "developer-links";
 $title = "LanguageTool";
 $title2 = "Links &amp; Resources for Developers";
-$lastmod = "2012-12-06 04:35:00 CET";
+$lastmod = "2012-12-31 14:35:00 CET";
 include("../../include/header.php");
 ?>
 
@@ -18,7 +18,7 @@ include("../../include/header.php");
           <li><?=show_simple_link("Archive (Sourceforge)", "http://sourceforge.net/mailarchive/forum.php?forum_name=languagetool-devel")?></li>
         </ul>
     </li>
-	<li>Subversion commit messages:
+    <li>Subversion commit messages:
         <ul>
           <li><?=show_simple_link("Subscribe/Unsubscribe", "http://lists.sourceforge.net/mailman/listinfo/languagetool-commits") ?></li>
           <li><?=show_simple_link("Archive (mail-archive.com)", "http://www.mail-archive.com/languagetool-commits@lists.sourceforge.net/")?></li>
@@ -36,7 +36,7 @@ include("../../include/header.php");
 <h3>Source Code and Development Versions</h3>
 
 <ul>
-	<li><?=show_simple_link("Source code in Subversion", "https://languagetool.svn.sourceforge.net/svnroot/languagetool/trunk/JLanguageTool")?></li>
+    <li><?=show_simple_link("Source code in Subversion", "https://languagetool.svn.sourceforge.net/svnroot/languagetool/trunk/JLanguageTool")?></li>
     <li><?=show_simple_link("Daily builds of the current development version", "/download/snapshots/") ?>
       (<?=show_simple_link("CHANGES.txt", "http://languagetool.svn.sourceforge.net/viewvc/languagetool/trunk/JLanguageTool/CHANGES.txt") ?>)</li>
     <li><?=show_simple_link("API documentation (Javadoc)", "/development/api/")?></li>
@@ -45,16 +45,16 @@ include("../../include/header.php");
 <h3>Development Tools and Resources</h3>
 
 <ul>
-	<li><?=show_simple_link("User interface translation (i18n) at Transifex", "http://www.transifex.net/projects/p/languagetool/")?></li>
-	<li><?=show_simple_link("Continuous integration at CloudBees", "http://languagetool.ci.cloudbees.com")?></li>
-	<li><?=show_simple_link("This website's usage statistics", "http://www.languagetool.org/stats")?></li>
-	<li><?=show_simple_link("LibreOffice bug reports related to LanguageTool", "https://bugs.freedesktop.org/buglist.cgi?quicksearch=%22language%20tool%22%20OR%20languagetool&amp;list_id=43548")?></li>
+    <li><?=show_simple_link("User interface translation (i18n) at Transifex", "http://www.transifex.net/projects/p/languagetool/resource/messagesbundleproperties/")?></li>
+    <li><?=show_simple_link("Continuous integration at CloudBees", "http://languagetool.ci.cloudbees.com")?></li>
+    <li><?=show_simple_link("This website's usage statistics", "http://www.languagetool.org/stats")?></li>
+    <li><?=show_simple_link("LibreOffice bug reports related to LanguageTool", "https://bugs.freedesktop.org/buglist.cgi?quicksearch=%22language%20tool%22%20OR%20languagetool&amp;list_id=43548")?></li>
 </ul>
 
 <h3>Grammar Error Collections and Corpora</h3>
 
 <ul>
-	<li><?=show_simple_link("HOO: Helping Our Own", "http://clt.mq.edu.au/research/projects/hoo/") ?></li>
+    <li><?=show_simple_link("HOO: Helping Our Own", "http://clt.mq.edu.au/research/projects/hoo/") ?></li>
     <li><?=show_simple_link("Learner corpora around the world", "http://www.uclouvain.be/en-cecl-lcworld.html") ?></li>
     <li><?=show_simple_link("LanguageTool's small German error collection", "http://languagetool.svn.sourceforge.net/viewvc/languagetool/trunk/JLanguageTool/src/main/resources/org/languagetool/resource/de/errors.txt?view=markup") ?></li>
     <li>English
@@ -66,13 +66,20 @@ include("../../include/header.php");
     </li>
 </ul>
 
+<h3>Other</h3>
+    
+<ul>
+    <li><?=show_simple_link("Building a regex engine — online resources", "http://stackoverflow.com/questions/1084069/building-a-regex-engine-online-resources")?></li>
+    <li><?=show_simple_link("OpenRegex", "https://github.com/knowitall/openregex")?> - an efficient and flexible token-based regular expression language and engine (GPLv3)</li>
+    <li><?=show_simple_link("GExp graph-expression", "http://code.google.com/p/graph-expression/")?> - high level automaton library for information extraction (Apache License)</li>
+</ul>
 
 <br /><br /><br />
 
 <p>Website design inspired by <!-- Please leave this if you use our template. Thank you -->
 <a href="http://www.darjanpanic.com" target="_blank"
-	title="Freelance Graphic artist">Darjan Panic</a> &amp; <a
-	href="http://www.briangreens.com" target="_blank">Brian Green</a> <!-- Please leave this if you use our template. Thank you -->
+    title="Freelance Graphic artist">Darjan Panic</a> &amp; <a
+    href="http://www.briangreens.com" target="_blank">Brian Green</a> <!-- Please leave this if you use our template. Thank you -->
 </p>
 
 <?php
