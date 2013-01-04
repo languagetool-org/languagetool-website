3ef58dc87c961bfa76236637cf400f9f313c57a5
Daniel Naber <naber@danielnaber.de>
2013-01-04 23:16:27 +0000

    update page with Maven artifact documentation

--- www/java-api/index.php
+++ www/java-api/index.php
@@ -3,24 +3,24 @@ $page = "development";
 $sub_page = "java-api";
 $title = "LanguageTool";
 $title2 = "Java API";
-$lastmod = "2012-10-10 19:20:00 CET";
+$lastmod = "2013-01-04 23:20:00 CET";
 include("../../include/header.php");
 include('../../include/geshi/geshi.php');
 ?>
 
 <h2 class="firstpara">Embedding LanguageTool in Java applications</h2>
 
-    <p>Get LanguageTool by downloading the *.zip package or by adding this dependency to your Maven pom.xml:</p>
+    <p>Get LanguageTool by downloading the *.zip package from the homepage or by adding this dependency to your Maven pom.xml:</p>
 
 <div class="xmlrule" style="margin-top:5px">
 <?php hl('<dependency>
   <groupId>org.languagetool</groupId>
   <artifactId>languagetool</artifactId>
-  <version>1.9</version>
+  <version>2.0.1</version>
 </dependency>'); ?>
 </div>
 
-    <p>The Maven artifact contains both code and resources for all languages and is thus quite large (46MB).</p>
+    <p>The Maven artifact contains both code and resources for all languages and is thus quite large (48MB).</p>
 
     <p>If you download the ZIP instead, you will need most JARs, <tt>org/languagetool/rules</tt> and <tt>org/languagetool/resources</tt>
     in your classpath. You can skip the following JARs as they are not needed at runtime:
@@ -29,7 +29,8 @@ include('../../include/geshi/geshi.php');
     <p>To use LanguageTool, you just need to create a <tt>JLanguageTool</tt> object and use that
 	to check your text. Also see <?=show_link("the API documentation", "/development/api/", 0) ?>. For example:</p>
 
-	<?php hljava('JLanguageTool langTool = new JLanguageTool(Language.ENGLISH);
+<div class="xmlrule" style="margin-top:5px">
+	<?php hljava('JLanguageTool langTool = new JLanguageTool(Language.BRITISH_ENGLISH);
 langTool.activateDefaultPatternRules();
 List<RuleMatch> matches = langTool.check("A sentence " +
     "with a error in the Hitchhiker\'s Guide tot he Galaxy");
@@ -40,8 +41,8 @@ for (RuleMatch match : matches) {
   System.out.println("Suggested correction: " +
       match.getSuggestedReplacements());
 }'); ?>
-	<br />
-
+</div>
+	
     <p>If you want spell checking, you will need to specify a language variant in the <tt>JLanguageTool</tt> constructor,
     e.g. <tt>Language.AMERICAN_ENGLISH</tt> instead of just <tt>Language.ENGLISH</tt>.</p>
 
