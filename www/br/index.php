abac0da3ab69b9040b2f84970f07bf1ab48bbc08
Dominique Pelle <dominique.pelle@gmail.com>
2013-02-07 08:28:58 +0000

    [br] minor change to Breton page.

--- www/br/index.php
+++ www/br/index.php
@@ -116,8 +116,7 @@ Trugarez ivez d’ar gevredigezh
 <a href="http://extensions.libreoffice.org/extension-center/an-drouizig-breton-spellchecker">geriadur brezhonek Hunspell</a>
 hag a zo implijet evit kavout fazioù skrivadurel gant LanguageTool.
 Diwallit avat: disheñvel eo difazier yezhadurel LanguageTool diouzh
-<a href="http://www.drouizig.org/index.php?option=com_content&view=category&id=38&Itemid=58&lang=br">difazier An Drouizig</a>.
-Arabat kemmeskañ anezho.</p>
+<a href="http://www.drouizig.org/index.php?option=com_content&view=category&id=38&Itemid=58&lang=br">hini An Drouizig</a>.</p>
 
 <h2>Aotre-implijout</h2>
 <p>
