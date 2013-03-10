82b213c803a28b41049db7c2e98e17907a95d7ee
Daniel Naber <naber@danielnaber.de>
2013-03-10 11:59:21 +0000

    keep snapshots for 10 days now; change the way we build so language-module.properties is correct

--- create-snapshot.sh
+++ create-snapshot.sh
@@ -10,12 +10,19 @@ cd svn-checkout
 M2_HOME=/home/languagetool/apache-maven-3.0.4
 M2=$M2_HOME/bin
 PATH=$M2:$PATH
-MAVEN_OPTS=-Xmx512m -XX:MaxPermSize=128m
+MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m"
 
-mvn clean package -DskipTests && 
- mv languagetool-office-extension/target/LanguageTool*.zip $SNAPSHOT_DIR/LanguageTool-`date +%Y%m%d`-snapshot.oxt &&
- mv languagetool-standalone/target/LanguageTool*.zip $SNAPSHOT_DIR/LanguageTool-`date +%Y%m%d`-snapshot.zip
+# We need to build like this - building the top-level project and then taking the
+# artifacts will aggregate *all* languages into the language-module.properties, even
+# for those projects that don't depend on all of the languages:
+mvn clean &&
+mvn --projects languagetool-office-extension --also-make package -DskipTests &&
+mv languagetool-office-extension/target/LanguageTool*.zip $SNAPSHOT_DIR/LanguageTool-`date +%Y%m%d`-snapshot.oxt &&
+mvn --projects languagetool-standalone --also-make package -DskipTests &&
+mv languagetool-standalone/target/LanguageTool*.zip $SNAPSHOT_DIR/LanguageTool-`date +%Y%m%d`-snapshot.zip &&
+mvn --projects languagetool-wikipedia --also-make package -DskipTests &&
+mv languagetool-wikipedia/target/LanguageTool*.zip $SNAPSHOT_DIR/LanguageTool-wikipedia-`date +%Y%m%d`-snapshot.zip
 
-# delete *.oxt files older than 5 days:
-rm `find $SNAPSHOT_DIR -name "*.oxt" -mtime +5`
-rm `find $SNAPSHOT_DIR -name "*.zip" -mtime +5`
+# delete files older than 10 days:
+rm `find $SNAPSHOT_DIR -name "*.oxt" -mtime +10`
+rm `find $SNAPSHOT_DIR -name "*.zip" -mtime +10`
