9144c3397f4fd7e37085567351c13d1441daed76
Jaume Ortola i Font <jaumeortola@gmail.com>
2013-04-04 21:50:42 +0000

    [web] Document enabledOnly=yes parameter in http server.

--- www/http-server/index.php
+++ www/http-server/index.php
@@ -58,9 +58,14 @@ include('../../include/geshi/geshi.php');
 	<tt>http://localhost:8081/?language=en&amp;disabled=STRANGE_RULE,ANOTHER_RULE&amp;text=my+text</tt><br />
 
 	<p>In this example, two rules will be disabled: <tt>STRANGE_RULE</tt> and <tt>ANOTHER_RULE</tt>. Note that there
-	should be no space after the comma. In contrast to the 
+	should be no space after the comma.</p> 
+	
+	<p>(Version 2.2) In contrast to the 
 	<a href="http://wiki.languagetool.org/command-line-options">command-line options</a>, in server mode you can specify both 
-	enabled and disabled rules at the same time.</p>
+	enabled and disabled rules at the same time. If you want to disable all the rules
+    except those explicitly enabled, you can use the parameter enabledOnly=yes, for example:</p>
+    
+    <tt>hhttp://localhost:8081/?language=en&enabled=STRANGE_RULE,ANOTHER_RULE&enabledOnly=yes&text=my+text</tt><br />
 
 	<p>Note that for a server started from a GUI, a user may configure it in the configuration dialog box to disable
 	some unwanted rules. This may be beneficial if the calling program does not allow configuration of the call to the
