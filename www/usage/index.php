1a9827cba78c435b5bc07cef2689dcc3990309ab
Daniel Naber <naber@danielnaber.de>
2013-06-08 09:10:40 +0000

    add link to the links page; tiny wording updates

--- www/usage/index.php
+++ www/usage/index.php
@@ -2,7 +2,7 @@
 $page = "usage";
 $title = "LanguageTool";
 $title2 = "Usage";
-$lastmod = "2013-06-01 20:40:00 CET";
+$lastmod = "2013-06-08 20:40:00 CET";
 include("../../include/header.php");
 include('../../include/geshi/geshi.php');
 ?>
@@ -10,7 +10,7 @@ include('../../include/geshi/geshi.php');
 <p class="firstpara">LanguageTool can be used in a different number of ways:</p>
 
 
-<h2>As a LibreOffice/OpenOffice.org extension</h2>
+<h2>As a LibreOffice/OpenOffice.org Extension</h2>
     <p>Double click the downloaded <tt>LanguageTool-2.x.oxt</tt> to install it.
   	If that doesn't work, call <em>Tools -&gt; Extension Manager -&gt; Add...</em>.
     Close LibreOffice/OpenOffice.org and re-start it. Type some text
@@ -19,9 +19,9 @@ include('../../include/geshi/geshi.php');
     to English for this example.</p>
 
 
-<h2>As a stand-alone application</h2>
+<h2>As a Stand-alone Application</h2>
 	<p>Download the *.zip version (not the *.oxt one) and unzip it.
-	Then start <tt>languagetool-standalone.jar</tt> by double clicking on it.
+	Then start <tt>languagetool-standalone.jar</tt> by double clicking it.
     If your computer isn't
 	configured to start *.jar archives, start it from the command line using
 	<tt>java -jar languagetool-standalone.jar</tt>.</p>
@@ -31,11 +31,11 @@ include('../../include/geshi/geshi.php');
 	cause the application to open and check the contents of the clipboard automatically. This way
 	you can use LanguageTool for applications that do not support direct integration of the checker.</p>
 
-    <p>By the way, the file size of the stand-alone version is much larger than that of the LibreOffice/OpenOffice
-    extension because it contains spell checking and thus dictionaries for most languages.</p>
+    <!--<p>By the way, the file size of the stand-alone version is much larger than that of the LibreOffice/OpenOffice
+    extension because it contains spell checking and thus dictionaries for most languages.</p>-->
 
 
-<h2>As a stand-alone application on the command line</h2>
+<h2>As a stand-alone Application on the Command Line</h2>
 	<p>See "as a stand-alone application" above, but start LanguageTool using:</p>
 	<tt>java -jar languagetool-commandline.jar &lt;filename&gt;</tt>
 	<p>Use <tt>java -jar languagetool-commandline.jar --help</tt> to get detailed usage information.</p>
@@ -48,6 +48,12 @@ include('../../include/geshi/geshi.php');
 
 <ul style="list-style-type: none">
     <li><?=show_link("Download the Add-On", "https://addons.mozilla.org/en-US/firefox/addon/languagetoolfx/", 0) ?></li>
+</ul>
+
+<h2>As an Add-On in other Programs</h2>
+
+<ul style="list-style-type: none">
+    <li>See the <?=show_link("links page", "../links/", 0) ?> for other software that can integrate LanguageTool</li>
 </ul>
 
 
