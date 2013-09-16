5bc7882c331d7ab0265ad04714533dd770579f32
Daniel Naber <naber@danielnaber.de>
2013-09-16 13:21:47 +0200

    some cleanup; adding link to easy fix issues

--- www/links/index.php
+++ www/links/index.php
@@ -2,7 +2,7 @@
 $page = "links";
 $title = "LanguageTool";
 $title2 = "Links &amp; Resources";
-$lastmod = "2013-04-30 20:35:00 CET";
+$lastmod = "2013-09-16 13:35:00 CET";
 include("../../include/header.php");
 ?>
 
@@ -10,6 +10,7 @@ include("../../include/header.php");
 
 <ul>
 	<li><?=show_simple_link("Open Bug Reports", "http://sourceforge.net/p/languagetool/bugs/")?></li>
+	<li><?=show_simple_link("Easy fixes", "https://github.com/languagetool-org/languagetool/issues?labels=easy+fix&state=open")?> - easy issues for new contributors</li>
 	<li><?=show_simple_link("Open Feature Requests", "http://sourceforge.net/p/languagetool/feature-requests/")?></li>
 	<li><?=show_simple_link("Create a new Bug Report", "https://sourceforge.net/p/languagetool/bugs/new/")?> or
         <?=show_simple_link("Feature Request", "https://sourceforge.net/p/languagetool/feature-requests/new/")?></li>
@@ -21,7 +22,7 @@ include("../../include/header.php");
 	<li><?=show_simple_link("LanguageTool for vim", "http://www.vim.org/scripts/script.php?script_id=3223") ?></li>
 	<li><?=show_simple_link("LanguageTool for LyX", "http://wiki.lyx.org/Tools/LyX-GrammarChecker") ?></li>
 	<li><?=show_simple_link("LanguageTool plugin for OmegaT", "https://sourceforge.net/projects/omegat-plugins/files/OmegaT-LanguageTool/")?>
-		a plugin that enables grammar-checking in computer-aided translation tool OmegaT (open source)</li>
+		a plugin that enables grammar-checking in computer-aided translation tool OmegaT</li>
 	<li><?=show_simple_link("LanguageTool in CheckMate", "http://www.opentag.com/okapi/wiki/index.php?title=CheckMate")?> used as a server to enhance translation QA</li>
 	<li><?=show_simple_link("LanguageToolFx for Firefox", "https://addons.mozilla.org/firefox/addon/languagetoolfx/")?></li>
         <li><?=show_simple_link("LanguageTool for Thunderbird", "https://addons.mozilla.org/thunderbird/addon/14781")?></li>
@@ -31,30 +32,30 @@ include("../../include/header.php");
 <h3>Other Open Source Grammar Checkers</h3>
 
 <ul>
-	<li><?=show_simple_link("After the Deadline", "http://open.afterthedeadline.com/")?>,
-		a grammar checker for English which integrates LanguageTool to support German and French</li>
-	<!-- <li><?=show_simple_link("LangBot", "http://apoema.net/langbot/en/gc.lb")?>,
-		an on-line grammar checker which uses LanguageTool, and includes integration
-		for Firefox via <?=show_simple_link("Ubiquity", "http://ubiquity.mozilla.com/") ?> plugin</li> -->				
 	<li><?=show_simple_link("An Gramad&oacute;ir", "http://borel.slu.edu/gramadoir/")?>,
 		a grammar checker for the Irish language</li>
-	<li><?=show_simple_link("CoGrOO", "http://cogroo.sourceforge.net/")?>
+	<li><?=show_simple_link("CoGrOO", "http://cogroo.sourceforge.net/")?>,
 		a grammar checker for Portuguese</li>
 	<li><?=show_simple_link("Grammalecte", "http://www.dicollecte.org/grammalecte/")?>
 		a grammar checker for French written in Python, based on Lightproof</li>
-	<li><?=show_simple_link("GRAC", "http://grac.sourceforge.net/")?>
-		corpus-based grammar checker written in Python</li>
     <li><?=show_simple_link("Lightproof", "http://numbertext.org/lightproof/")?>,
    		a Python-based grammar checker embedded in LibreOffice since 3.5,
         <?=show_simple_link("Lightproof editor", "http://extensions.libreoffice.org/extension-center/lightproof-editor")?>
     </li>
     <li><?=show_simple_link("Link Grammar", "http://www.abisource.com/projects/link-grammar/")?>, a syntactic parser for English</li>
-	<li><?=show_simple_link("Queequeg", "http://queequeg.sourceforge.net/index-e.html")?>,
-		agreement checker written in Python</li>
 	<li><?=show_simple_link("Virastyar", "https://sourceforge.net/projects/virastyar/")?>,
 		spelling and punctuation checker for Persian for MS Word, written in C#</li>		
-	<li><?=show_simple_link("Old version of LanguageTool in Python", "http://tkltrans.sourceforge.net/#r03") ?>, a much older
-		and less powerful version without OpenOffice.org integration</li>
+    <li>Unmaintained software:
+        <ul>
+            <li><?=show_simple_link("After the Deadline", "http://open.afterthedeadline.com/")?>,
+                a grammar checker for English which integrates LanguageTool to support German and French</li>
+            <li><?=show_simple_link("GRAC", "http://grac.sourceforge.net/")?>,
+                corpus-based grammar checker written in Python</li>
+            <li><?=show_simple_link("Queequeg", "http://queequeg.sourceforge.net/index-e.html")?>, agreement checker written in Python</li>
+            <li><?=show_simple_link("Old version of LanguageTool in Python", "http://tkltrans.sourceforge.net/#r03") ?>, a much older
+                and less powerful version without OpenOffice.org integration</li>
+        </ul>
+    </li>
 </ul>
 
 <?php
