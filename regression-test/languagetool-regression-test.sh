cac568b26fbdbb5277d23338d328ce73976d3649
Daniel Naber <naber@danielnaber.de>
2013-10-02 09:56:37 +0200

    also run on Portuguese data

--- regression-test/languagetool-regression-test.sh
+++ regression-test/languagetool-regression-test.sh
@@ -36,7 +36,7 @@ languageToolVersion=`java -jar $jarFile version`
 echo "Version: $languageToolVersion</p>" >>$globalResultFile
 
 # as this uses a lot of resources, we only check the languages in active development:
-for lang in en de fr ru br ca pl it
+for lang in en de fr ru br ca pl it pt
 do
   echo "============== $lang =============="
   wikiFile="$corpusDir/$lang/${lang}wiki-[0-9]*-pages-articles.xml"
