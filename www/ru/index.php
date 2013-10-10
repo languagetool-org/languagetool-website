4a9b41696a375e2ebe43549e75dff3bf2058e168
yakovru <yakovr_st@mail.ru>
2013-10-10 14:55:40 +0400

    Update index.php
    version LO\AOOo

--- www/ru/index.php
+++ www/ru/index.php
@@ -2,7 +2,7 @@
 $page = "ru";
 $title = "LanguageTool";
 $title2 = "Программа для проверки грамматики и стиля";
-$lastmod = "2013-10-02 08:22:00 CET";
+$lastmod = "2013-10-10 08:52:00 CET";
 $enable_textcheck = 1;
 $enable_fancybox = 1;
 include("../../include/header.php");
@@ -128,9 +128,7 @@ include("../../include/checkform.php");
     <li>Нажать «ОК».</li>
     <li>Обязательно перезапустить LibreOffice/OpenOffice.Org (включая и быстрый запуск).</li>
 </ul>
-<p>Системные требования: как минимум OpenOffice.org 3.0.1 (или <a href="http://ru.libreoffice.org/">LibreOffice</a>), <a href="http://www.java.com/ru/download/manual.jsp">Java 7</a>. 
-</p>
-<p>Рекомендуется как минимум Apache OpenOffice 3.4.1 (или LibreOffice 3.5.4). 
+<p>Системные требования: как минимум (или <a href="http://ru.libreoffice.org/">LibreOffice 3.5.4</a>) или Apache OpenOffice 3.4.1+, <a href="http://www.java.com/ru/download/manual.jsp">Java 7</a>. 
 </p>
 <p>LibreOffice 3.5 и старше уже включает в себя лёгкую систему проверки грамматики (LightProof), которая блокирует работу LanguageTool для английского и русского языков.
 Отключить LightProof и включить LanguageTool для указанных языков можно через меню   Сервис -> Параметры -> LibreOffice -> Настройки языка -> Лингвистика -> Доступные языковые модули -> Правка ...
