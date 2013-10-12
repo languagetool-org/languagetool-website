0c48ba72bb681eb1c01cc81f1b24ba5cb8905dc7
Dominique Pelle <dominique.pelle@gmail.com>
2013-10-12 10:05:24 +0200

    [br] added "Stefan Carpentier" in the list of people      who helped improve LanguageTool in Breton.

--- www/br/index.php
+++ www/br/index.php
@@ -116,8 +116,8 @@ pe skrivit er <a href="http://www.languagetool.org/forum/">forom</a>.</p>
 <h2>Trugarez</h2>
 <p>
 Trugarez d’ar re o deus sikouret da sevel LanguageTool e brezhoneg:
-Fulup Jakez, Joseph Monfort, Denis Arnaud, Thierry Vignaud ha
-Philippe Basciano-Le Gall.</p>
+Fulup Jakez, Joseph Monfort, Denis Arnaud, Thierry Vignaud,
+Philippe Basciano-Le Gall ha Stefan Carpentier.</p>
 <p>
 Ne vefe ket posupl an difazier LanguageTool hep ar geriadur
 brezhonek Apertium. Trugarez neuze da Francis Tyers ha
