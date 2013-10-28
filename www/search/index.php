bb049e8bf3cddc3f24f445e18e0ad1cdb961e191
Daniel Naber <naber@danielnaber.de>
2013-10-28 10:12:06 +0100

    use the search from master.com

--- www/search/index.php
+++ www/search/index.php
@@ -6,21 +6,27 @@ $show_date = 0;
 include("../../include/header.php");
 ?>
 
-<p>This is a test, please move on...</p>
-
-<form accept-charset="UTF-8" action="http://www.jrank.org/api/search/v2" method="get">
-    <div style="margin:0;padding:0;display:inline"><input name="utf8" type="hidden" value="✓" /></div>
-    <fieldset style="border: 1px solid rgb(175, 175, 175); display: inline;">
-        <legend><a href="http://www.jrank.org/" style="font-size: 10pt; font-family: Arial, sans-serif;">Site Search</a></legend>
-        <a href="http://www.jrank.org/"><img alt="Site Search" src="http://www.jrank.org/images/jrank_88_31-fs8.png" style="border: none; vertical-align: middle;" title="Site Search" /></a>
-        <input id="key" name="key" type="hidden" value="b4434460d2e23b942cfded2b6c8c6c04df3e3800" />
-        <input name="ie_utf8_fix" type="hidden" value="☠" />
-        <input id="q" name="q" style="display: inline; vertical-align: middle;" type="text" value="" />
-        <input name="commit" style="display: inline; vertical-align: middle;" type="submit" value="Search" />
-    </fieldset>
-</form>
+<TABLE WIDTH="100%">
+    <FORM name="searchform" METHOD=GET ACTION="http://languagetool.org.master.com/texis/master/search/">
+        <TR><TD ALIGN=CENTER width="80%" NOWRAP><INPUT SIZE=30 name="q" value="" placeholder="search terms"><INPUT TYPE=submit name=xsubmit VALUE="Search:" id="mastercom-but_search" class="mastercom-searchbut"><SELECT NAME=s ONCHANGE="var val = this.form.s.options[this.form.s.selectedIndex].value; if (val != 'SS') this.form.submit()">
+                        <OPTION VALUE=SS SELECTED>All of Languagetool.org
+                        <OPTION VALUE=.1 >Only Javadoc
+                        <OPTION VALUE=.2 >Only Wiki
+                            <!--{Services}--></SELECT></TD>
+            <TD ALIGN=RIGHT WIDTH="10%"><A HREF="http://www.master.com/texis/master/app/home.html" target="_top"><IMG WIDTH=96 HEIGHT=26 SRC="http://www.master.com/img/ms.gif" ALT="master.com" BORDER=0></A></TD>
+        </TR>
+    </FORM>
+</TABLE>
 
 <div style="height:500px"/>
+
+<script type="text/javascript">
+    <!--
+    if (document.searchform && document.searchform.q) {
+        document.searchform.q.focus();
+    }
+    // -->
+</script>
 
 <?php
 include("../../include/footer.php");
