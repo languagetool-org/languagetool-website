2e714e938340e152f1093d8aceb63ae62ceeefbd
Daniel Naber <naber@danielnaber.de>
2013-11-11 22:59:27 +0100

    increase perm size, trying to stabilize nightly build

--- create-snapshot.sh
+++ create-snapshot.sh
@@ -2,7 +2,7 @@
 # compiles the current LanguageTool code and moves the result to a snapshots directory
 
 export JAVA_HOME=/home/languagetool/jdk1.7.0_07
-export MAVEN_OPTS="-Xmx500M -XX:MaxPermSize=256M"
+export MAVEN_OPTS="-Xmx500M -XX:MaxPermSize=300M"
 
 SNAPSHOT_DIR=../languagetool-website/www/download/snapshots
 
