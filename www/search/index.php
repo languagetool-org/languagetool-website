b85a3899e4e06fd9aaf016d9e88911622c36fd04
Daniel Naber <naber@danielnaber.de>
2014-01-03 12:56:55 +0100

    disable search feature, master.com seemed to have problems with our switch to SSL

--- www/search/index.php
+++ www/search/index.php
@@ -12,27 +12,7 @@
 
 <div id="textcontent">
     
-<TABLE WIDTH="100%">
-    <FORM name="searchform" METHOD=GET ACTION="http://languagetool.org.master.com/texis/master/search/">
-        <TR><TD ALIGN=CENTER width="80%" NOWRAP><INPUT SIZE=30 name="q" value="" placeholder="search terms"><INPUT TYPE=submit name=xsubmit VALUE="Search:" id="mastercom-but_search" class="mastercom-searchbut"><SELECT NAME=s ONCHANGE="var val = this.form.s.options[this.form.s.selectedIndex].value; if (val != 'SS') this.form.submit()">
-                        <OPTION VALUE=SS SELECTED>All of languagetool.org
-                        <OPTION VALUE=.1 >Only Javadoc
-                        <OPTION VALUE=.2 >Only Wiki
-                            <!--{Services}--></SELECT></TD>
-            <TD ALIGN=RIGHT WIDTH="10%"><A HREF="http://www.master.com/texis/master/app/home.html" target="_top"><IMG WIDTH=96 HEIGHT=26 SRC="http://www.master.com/img/ms.gif" ALT="master.com" BORDER=0></A></TD>
-        </TR>
-    </FORM>
-</TABLE>
-
-<div style="height:500px"></div>
-
-<script type="text/javascript">
-    <!--
-    if (document.searchform && document.searchform.q) {
-        document.searchform.q.focus();
-    }
-    // -->
-</script>
+Sorry, the full text search is currently not available
 
 </div>
 
