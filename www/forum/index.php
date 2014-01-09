f42e2c4ec68fde2f1156822da001b217b8ce896c
Daniel Naber <naber@danielnaber.de>
2014-01-09 12:27:02 +0100

    embedding the forum in our SSL site doesn't work, so redirect user to Nabble

--- www/forum/index.php
+++ www/forum/index.php
@@ -1,22 +1,4 @@
-<!doctype html>
-<html lang=en>
-<head>
-    <?php
-    $page = "forum";
-    $title = "LanguageTool Forum";
-    ?>
-    <?php include("../../include/header.php"); ?>
-</head>
-<body>
-<?php include("../../include/partials/nav.php"); ?>
-
-<a id="nabblelink" href="http://languagetool.2306527.n4.nabble.com/">Go to the LanguageTool user forum</a>
-<script src="http://languagetool.2306527.n4.nabble.com/embed/f3993201"></script>
-
 <?php
-$show_date = 0;
-include("../../include/footer.php");
+header("Location: http://languagetool.2306527.n4.nabble.com/");
+exit;
 ?>
-
-</body>
-</html>
