da9bc297db43a5f7da5b9ccbd8a1fd622e6611fc
yakovru <yakovr_st@mail.ru>
2014-01-10 12:04:19 -0800

    [ru] update footer

---
+++ www/ru/include/start_actions.php
@@ -0,0 +1,13 @@
+<div id="actions">
+  <div class="actions-inner">
+    <div id="license_info">
+      <div class="inner">
+        <h2>Лицензия и исходный код</h2>
+        <p>
+          LanguageTool LanguageTool свободно распространятся по лицензии <a href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1">LGPL 2.1</a> или более новой.<br/><a href="https://github.com/languagetool-org/">Исходные коды</a> размещены на сервисе github.<br/>Содержимое этой домашней страницы доступно по лицензии <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0</a>.
+        </p>
+      </div>
+    </div>
+    <div style="clear:both;"></div>
+  </div>
+</div>
