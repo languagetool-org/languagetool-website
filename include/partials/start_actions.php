0e37041faa35e17e0a6d89e1c89891580b8ec67f
Daniel Naber <naber@danielnaber.de>
2014-01-13 19:43:36 +0100

    small HTML syntax fix

--- include/partials/start_actions.php
+++ include/partials/start_actions.php
@@ -13,7 +13,7 @@
         <h2>Get announcements via email</h2>
         <p>
           <form action="https://eu1.cleverreach.com/f/46260-106283/wcs/" method="post">
-              <input id="text2133452" name="email" id="email" type="text" placeholder="email address"><input id="submit" type="submit" value="Subscribe">
+              <input id="text2133452" name="email" type="text" placeholder="email address"><input id="submit" type="submit" value="Subscribe">
           </form>
         </p>
       </div>
