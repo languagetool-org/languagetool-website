16e42c7f6d7932da266226b79b14c12564496c5c
Daniel Naber <naber@danielnaber.de>
2014-01-22 10:41:47 +0100

    make more text translatable

--- include/pages/downloads.php
+++ include/pages/downloads.php
@@ -10,9 +10,24 @@
     
   <br>
   <p class="small">
-    Please see <a href="/issues/">our checklist</a> if you experience problems.
-      Download <a href="/download/">old releases</a> or <a href="/download/snapshots/?C=M;O=D">daily builds</a>.
-      Start with <a href="/webstart/web/LanguageTool.jnlp">Java WebStart</a>.
+      <?php if(isset($checklistText)) { ?>
+          <?php print $checklistText ?>
+      <?php } else { ?>
+          Please see <a href='/issues/'>our checklist</a> if you experience problems.
+      <?php } ?>
+
+      <?php if(isset($otherDownloadsText)) { ?>
+          <?php print $otherDownloadsText ?>
+      <?php } else { ?>
+          Download <a href="/download/">old releases</a> or <a href="/download/snapshots/?C=M;O=D">daily builds</a>.
+      <?php } ?>
+
+      <?php if(isset($webstartText)) { ?>
+          <?php print $webstartText ?>
+      <?php } else { ?>
+          Start with <a href="/webstart/web/LanguageTool.jnlp">Java WebStart</a>.
+      <?php } ?>
+      
   </p>
 
 </div>
--- www/de/index.php
+++ www/de/index.php
@@ -54,6 +54,9 @@
     $downloadTitle = "LanguageTool für <strong>LibreOffice/OpenOffice</strong>";
     $downloadTitleStandAlone = "LanguageTool als <strong>Desktop-Programm</strong>";
     $downloadLabelFx = "LanguageTool als Erweiterung für <strong>Firefox</strong>";
+    $checklistText = "Probleme? Siehe <a href='/issues/'>unsere Hilfeseite</a>.";
+    $otherDownloadsText = "<a href='/download/'>Ältere Versionen</a> und <a href='/download/snapshots/?C=M;O=D'>täglich neu gebaute Versionen</a> herunterladen.";
+    $webstartText = "LanguageTool <a href='/webstart/web/LanguageTool.jnlp'>mit Java WebStart ausführen</a>.";
     
     // ------------- TRANSLATIONS END HERE -------------
     ?>
