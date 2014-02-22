69c5686d62c01a13a9142f833626624eeea596e8
Daniel Naber <naber@danielnaber.de>
2014-02-22 20:56:54 +0100

    small layout improvements

--- www/contact/index.php
+++ www/contact/index.php
@@ -12,33 +12,34 @@
 
 <div id="textContent">
 
-    <h1>Need Help?</h1>
+    <h2>Need Help?</h2>
     
     <p>Please visit the <a href="/support/">support page</a>.</p>
-
-    <h1>Imprint</h1>
+    
+    
+    <h2>Imprint</h2>
     
     <p>
         Daniel Naber<br/>
         Friesenstr. 3<br/>
         14482 Potsdam<br/>
-        email: <span style="color:#777777">naber <span>a&#116;</span> danielnaber<span>.</span>de</span>
+        email: <span style="color:#777777">naber <span>a&#116;</span> danielnaber<span>.</span>de</span><br/>
+        homepage: <a href="http://www.danielnaber.de">danielnaber.de</a>
     </p>
 
-    <h1>Licenses</h1>
+    
+    <h2>Licenses</h2>
 
     <p>The contents of this homepage is available under <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC BY-SA 3.0</a>.
-        The software, LanguageTool, is available under <a href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1">LGPL 2.1</a> or later.</p>
-
-    <p>This page uses some icons provided by <a  href="http://www.fatcow.com/free-icons">FatCow</a>
+        The software, LanguageTool, is available under <a href="http://www.gnu.org/licenses/old-licenses/lgpl-2.1">LGPL 2.1</a> or later.
+        This page uses some icons provided by <a  href="http://www.fatcow.com/free-icons">FatCow</a>
         which are available under <a href="http://creativecommons.org/licenses/by/3.0/us/">CC BY 3.0</a>.
-        <!-- see images/(information|exclamation|accept).png in ltcommunity --></p>
-    
-    <p>The font used is <a href="https://www.google.com/fonts/specimen/Source+Sans+Pro">Source Sans Pro</a> from Adobe,
-    available under <a href="http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">SIL Open Font License, 1.1</a>.</p>
+        <!-- see images/(information|exclamation|accept).png in ltcommunity -->
+        The font used is <a href="https://www.google.com/fonts/specimen/Source+Sans+Pro">Source Sans Pro</a> from Adobe,
+        available under <a href="http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">SIL Open Font License, 1.1</a>.</p>
 
 
-    <h1>Privacy</h1>
+    <h2>Privacy</h2>
 
     <p>This website uses <a href="http://www.piwik.org">Piwik</a> for web analytics. We're shortening your
         IP address (to a form like 192.168.xxx.xxx) to protect your privacy. If you don't want your
