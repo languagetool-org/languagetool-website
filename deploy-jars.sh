b20404bab1da39fdafb7fcef3cb11181dfdaad61
Daniel Naber <naber@danielnaber.de>
2014-03-11 13:22:02 +0100

    trying to fix auto-deploy: LT wikipedia JAR was not replaced

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -3,7 +3,7 @@
 # Run this after a new snapshot has been created.
 # dnaber, 2014-03-04
 
-SNAPSHOT_DIR=../languagetool-website/www/download/snapshots
+SNAPSHOT_DIR=/home/languagetool/languagetool.org/languagetool-website/www/download/snapshots
 WEB_LIB=/home/languagetool/tomcat/webapps/ROOT/WEB-INF/lib
 DATE=`date +%Y%m%d`
 
@@ -19,10 +19,11 @@ rm -r /tmp/lt-wikipedia-snapshot
 
 SNAPSHOT=$SNAPSHOT_DIR/LanguageTool-$DATE-snapshot.zip
 if [ ! -f $SNAPSHOT ]; then
-  echo "$SNAPSHOT not found, stopping"
+  echo "Error: $SNAPSHOT not found, stopping"
   exit
 fi
 
+echo "Going to unzip: $SNAPSHOT"
 unzip -d /tmp/lt-snapshot $SNAPSHOT
 
 # backup of libs:
@@ -46,7 +47,10 @@ rm $WEB_LIB/languagetool-core-*-SNAPSHOT.jar
 mkdir -p /home/languagetool/tomcat/webapps/ROOT/WEB-INF/classes/META-INF/org/languagetool/
 cp /tmp/lt-snapshot/LanguageTool-*-SNAPSHOT/META-INF/org/languagetool/language-module.properties /home/languagetool/tomcat/webapps/ROOT/WEB-INF/classes/META-INF/org/languagetool/
 
-unzip -d /tmp/lt-wikipedia-snapshot $SNAPSHOT_DIR/LanguageTool-wikipedia-$DATE-snapshot.zip
+$WIKI_JAR=$SNAPSHOT_DIR/LanguageTool-wikipedia-$DATE-snapshot.zip
+rm $WEB_LIB/languagetool-wikipedia-*-SNAPSHOT.jar
+echo "Going to unzip: $WIKI_JAR"
+unzip -d /tmp/lt-wikipedia-snapshot $WIKI_JAR
 cp /tmp/lt-wikipedia-snapshot/LanguageTool-*-SNAPSHOT/languagetool-wikipedia.jar $WEB_LIB
 
 cd /home/languagetool
