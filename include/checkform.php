efdc297b50386cf40a5c58e5d8c45636cbec5fb3
Daniel Naber <naber@danielnaber.de>
2014-10-27 12:34:34 +0100

    avoid PHP warning

--- include/checkform.php
+++ include/checkform.php
@@ -71,7 +71,7 @@ function printLangOption($langCode) {
         <p id="checktextpara" style="margin: 0">
             <?php
             require_once("default_texts.php");
-            if ((strpos($_SERVER['HTTP_REFERER'], "http://www.openthesaurus.de") === 0 || strpos($_SERVER['HTTP_REFERER'], "https://www.openthesaurus.de") === 0) 
+            if (isset($_SERVER['HTTP_REFERER']) && (strpos($_SERVER['HTTP_REFERER'], "http://www.openthesaurus.de") === 0 || strpos($_SERVER['HTTP_REFERER'], "https://www.openthesaurus.de") === 0) 
                 && $_POST['text']) {
               // This is just a test! Please don't rely on this!
               $checkDefaultText = htmlspecialchars($_POST['text']);
