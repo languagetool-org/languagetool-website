5b2c6abc04467fc4beb144f000cd9f8f6b028d2f
yakovru <yakovr_st@mail.ru>
2015-03-30 23:10:54 +0300

    [ru] Update info

--- www/ru/index.php
+++ www/ru/index.php
@@ -80,7 +80,7 @@
 
 <h2>Функциональность</h2>
 
-В состав LanguageTool входит <a href="http://community.languagetool.org/rule/list?lang=ru"> 370 правил</a> для проверки русскоязычных текстов.<br/><br/>
+В состав LanguageTool входит <a href="http://community.languagetool.org/rule/list?lang=ru"> 430 правил</a> для проверки русскоязычных текстов.<br/><br/>
 Правила разделены на группы:
 <br/>
 <!--
