fbe27af244b1b1216775016045524bf2f47e0c08
Daniel Naber <naber@danielnaber.de>
2015-07-03 14:32:11 +0200

    mention low traffic

--- www/contact/newsletter.php
+++ www/contact/newsletter.php
@@ -14,7 +14,7 @@
 
     <h1>Newsletter</h1>
     
-    <p>Subscribe to our newsletter to stay up-to-date on new releases and other major LanguageTool-related events.</p>
+    <p>Subscribe to our newsletter to stay up-to-date on new releases and other major LanguageTool-related events (4-8 messages per year):</p>
 
     <form action="https://eu1.cleverreach.com/f/46260-106283/wcs/" method="post">
         <input autofocus size="30" id="text2133452" name="email" type="text" placeholder="you@provider.org">
