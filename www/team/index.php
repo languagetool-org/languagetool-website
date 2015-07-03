8f26081e26865c386b045ec148e64ae01540293c
Daniel Naber <naber@danielnaber.de>
2015-07-03 16:13:07 +0200

    trying to list all contributors

--- www/team/index.php
+++ www/team/index.php
@@ -14,18 +14,17 @@
     
     <h1>Team</h1>
 
+    <p>LanguageTool is an Open Source application developed by a group of language enthusiasts and software developers.</p>
+
     <h2>Want to join us?</h2>
     
     <p>Please subscribe to our <a href="../development/mailing-list.php">development
     mailing list</a>, where all the discussion happens. Subscribe to the <a href="../contact/newsletter.php">newsletter</a>
     for information about new releases. If you have a quick question you may want to
     use the <a href="/forum">forum</a> instead.</p>
-    
-    
-    <h2>Team</h2>
-    
-    <p>LanguageTool is an Open Source application developed by a group of language enthusiasts and software developers.
-    The people in the core team are:</p>
+
+
+    <h2>Core Team</h2>
 
     <p><a href="http://it.linkedin.com/in/paolobianchini/">Paolo Bianchini</a>,
     Markus Brenneis,
@@ -47,6 +46,126 @@
     
     <p>See the <a href="/languages">list of supported languages</a> for information about who maintains the error
     detection rules for each language.</p>
+
+    <h2>Contributors</h2>
+    
+    <p>The following people have contributed rules or source code to LanguageTool. Thank you!</p>
+    
+    <!-- source: our changelog and git log searched for "by", "thanks", and "contribute" -->
+    <ul style="float:left">
+        <li>Agnese Dal Borgo
+        <li>Agnes Souque
+        <li>Albert Jané
+        <li>Albrecht Müller
+        <li>Alexander Socop Arango
+        <li>Alex Buloichik
+        <li>Alireza Eskandarpour Shoferi
+        <li>Allan Borra
+        <li>Ankit
+        <li>Annika Nietzio
+        <li>Anton Karl Ingason
+        <li>Arle Lommel
+        <li>atrons
+        <li>Camille Moulin
+        <li>Dean Doscher
+        <li>Denis Arnaud
+        <li>Derek M Jones
+        <li>Dmitri Gabinski
+        <li>Ebrahim Byagowi
+        <li>Edward Sanford Sutton
+        <li>Eike Rathke
+        <li>Elena Cejas
+        <li>Esben Aaberg
+        <li>Fabio Rainone
+        <li>Felix Sasaki and his students
+        <li>Fulup Jakez
+        <li>Heikki Lehvaslaiho
+        <li>Henrik Bendt
+        <li>Hugo Voisard
+        <li>Ilona Kuzmickaja
+        <li>Ionuț Păduraru
+        <li>Irvine
+        <li>jab
+        <li>Jarek Lipski
+        <li>Jimmy O'Regan
+        <li>Jitesh V. S.
+    </ul>
+
+    <ul style="float:left">
+        <li>John Andrews
+        <li>Jordi Mas
+        <li>Joseph Monfort
+        <li>Jozef Licko
+        <li>Julian von Heyl
+        <li>Julia Semenenko
+        <li>Kevin Scannell
+        <li>Kira Goncharova
+        <li>Kumara Bhikkhu
+        <li>Maksym Davydov
+        <li>Mantas Kriaučiūnas
+        <li>Martin Srebotnjak
+        <li>Metathesis
+        <li>Michael Baumann
+        <li>Michael Bryant
+        <li>mik09
+        <li>Mike Detwiler
+        <li>Mike Unwalla
+        <li>Mility
+        <li>Nathaniel Oco
+        <li>Nathan Wells
+        <li>Nick Hough
+        <li>Niklas Johansson
+        <li>NOKUBI Takatsugu
+        <li>Oliver Schlöbe
+        <li>Olivier R.
+        <li>OpenTaal
+        <li>paolobenve
+        <li>Pax
+        <li>Pere Farrando
+        <li>Petra Galuscakova
+        <li>Petr Mladek
+        <li>Philippe Basciano-Le Gall
+        <li>PhilippeW
+        <li>Ramon Torrents
+        <li>Rémy Léone
+    </ul>
+    
+    <ul>
+        <li>Reza1615
+        <li>Ricard Roca
+        <li>Riccardo Murri
+        <li>Salino01
+        <li>Sander van Geloven
+        <li>Serkan Kaba
+        <li>Shameera
+        <li>Shugyousha
+        <li>Silvan Jegen
+        <li>Sławek Borewicz
+        <li>Srinath Warrier
+        <li>Stefan Carpentier
+        <li>sumit
+        <li>Susana Sotelo Docio
+        <li>TaalTik
+        <li>Takahiro Shinkai
+        <li>Tao Lin
+        <li>Thai
+        <li>thatkookooguy
+        <li>Thierry Vignaud
+        <li>Tugdual Kalvez
+        <li>uma
+        <li>Viljar
+        <li>Wolfgang Lenhard
+        <li>Xesús González Rato
+        <li>yeryry
+        <li>Zdenko Podobný
+    </ul>
+    
+    <p style="clear: both"></p>
+
+    <p>LanguageTool has a history of more than 10 years, so we will probably have missed some
+    contributors. If you should be on the list, please let us know and we'll add you.</p>
+    
+    <!-- TODO: external dependencies -->
 
 
     <h2>Need Help?</h2>
