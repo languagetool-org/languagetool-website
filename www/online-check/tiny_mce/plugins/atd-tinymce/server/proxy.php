e132eab2d5b640b5978c051cdd5fc8cf6f403489
Daniel Naber <naber@danielnaber.de>
2015-11-11 12:35:51 +0100

    more try/finally usage for better robustness

--- www/online-check/tiny_mce/plugins/atd-tinymce/server/proxy.php
+++ www/online-check/tiny_mce/plugins/atd-tinymce/server/proxy.php
@@ -14,33 +14,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $postText = html_entity_decode($postText, ENT_COMPAT, "UTF-8");
   
   $curl = curl_init();
-  register_shutdown_function('shutdown', $curl);
-  curl_setopt($curl, CURLOPT_URL, "https://languagetool.org");
-  curl_setopt($curl, CURLOPT_PORT, 8081);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $postText);
-  curl_setopt($curl, CURLOPT_HEADER, 0);
-  if (isset($_SERVER['HTTP_REFERER'])) {
-    curl_setopt($curl, CURLOPT_REFERER, $_SERVER['HTTP_REFERER']);
-  }
-  $realIp = $_SERVER['REMOTE_ADDR'];
-  curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-forwarded-for: $realIp"));
-
-  header("Content-Type: text/xml; charset=utf-8");
-  //for debugging:
-  //header("Content-Type: text/plain");
-  
-  if (curl_exec($curl) === false) {
-    $errorMessage = curl_error($curl);
-    $errorCode = curl_errno($curl);
-    if (!$errorMessage && $errorCode == 7) {
-      // Actually the message can be empty, e.g. if the Apache server is in "No buffer space available" state...
-      $errorMessage = "Could not connect LanguageTool server";   // source: http://curl.haxx.se/libcurl/c/libcurl-errors.html
+  try {
+    register_shutdown_function('shutdown', $curl);
+    curl_setopt($curl, CURLOPT_URL, "https://languagetool.org");
+    curl_setopt($curl, CURLOPT_PORT, 8081);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $postText);
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    if (isset($_SERVER['HTTP_REFERER'])) {
+      curl_setopt($curl, CURLOPT_REFERER, $_SERVER['HTTP_REFERER']);
     }
-    print "Error: " . $errorMessage;
-    error_log("proxy.php error: $errorMessage, code: $errorCode");
+    $realIp = $_SERVER['REMOTE_ADDR'];
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array("X-forwarded-for: $realIp"));
+  
+    header("Content-Type: text/xml; charset=utf-8");
+    //for debugging:
+    //header("Content-Type: text/plain");
+    if (curl_exec($curl) === false) {
+      $errorMessage = curl_error($curl);
+      $errorCode = curl_errno($curl);
+      if (!$errorMessage && $errorCode == 7) {
+        // Actually the message can be empty, e.g. if the Apache server is in "No buffer space available" state...
+        $errorMessage = "Could not connect LanguageTool server";   // source: http://curl.haxx.se/libcurl/c/libcurl-errors.html
+      }
+      print "Error: " . $errorMessage;
+      error_log("proxy.php error: $errorMessage, code: $errorCode");
+    }
+  } finally {
+    curl_close($curl);
   }
-  curl_close($curl);
 } else {
   print "Error: this proxy only supports POST requests";
 }
