f4a4ce424047354608d1aa57434c88b30945fe93
Daniel Naber <naber@danielnaber.de>
2015-11-19 21:15:50 +0100

    remove duplicated function definition

--- include/header.php
+++ include/header.php
@@ -109,12 +109,6 @@
 <?php if (isset($enable_download_dialogs) && $enable_download_dialogs) { ?>
   <script type="text/javascript" src="<?= getRoot() ?>/js/zebra_dialog.js"></script>
   <script type="text/javascript">
-    function installChromeExtension() {
-        chrome.webstore.install();
-        //debug: chrome.webstore.install('', function() {alert('success');}, function(e) {alert('fail:'+e);});
-        return false;
-    }
-    
     function showDownloadOfficeThanks() {
         $.Zebra_Dialog('<b>Thanks for downloading LanguageTool!</b>' +
             '<ul>' +
