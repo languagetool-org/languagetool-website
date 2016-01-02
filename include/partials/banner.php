bae8c0a2660ec01f8d0578fe97e95721183212b8
Daniel Naber <naber@danielnaber.de>
2016-01-02 12:09:02 +0100

    use the marketing text again - the "growth hacker" job had zero feedback

--- include/partials/banner.php
+++ include/partials/banner.php
@@ -1,3 +1,4 @@
 <div style="background:#fee68a;padding:8px;color:black;text-align: center">
-    <a href="/job/growth_hacker.php">Help us to grow LanguageTool</a>
+    <!--<a href="/job/growth_hacker.php">Help us to grow LanguageTool</a>-->
+    <a href="/job/marketing.php">Help us to market LanguageTool</a>
 </div>
--- www/job/marketing.php
+++ www/job/marketing.php
@@ -14,17 +14,22 @@
 
     <h2>Job: Marketing LanguageTool</h2>
     
-    <p>LanguageTool is looking for someone who takes care of its marketing.
+    <p>The LanguageTool project is looking for a volunteer who takes care of its marketing.
     What would you do? Your task would simply be:</p>
 
     <p><strong>Make LanguageTool and its add-ons more popular.</strong></p>
 
     <p>How you do that is up to you. You could blog, improve our website, or create a long-term concept for marketing and 
-    then help making it reality. Like the developers, you won't get paid with money but with fame and a languagetool.org
-    email address. You either have experience in marketing or are eager to learn. You don't start from zero: LanguageTool
-    has a few hundred followers on twitter and more than 10,000 visits to its homepage per day. You should be able to
-    regularly spend at least two hours per week on it. Would you like to take this job? Then please contact
-    daniel.naber ..at.. languagetool.org.
+    then help make it reality. Like the developers, you won't get paid with money but with the chance to be part
+    of the team that develops the world's most powerful Open Source style and grammar checker (and a languagetool.org
+    email address).</p>
+    
+    <p>You either have experience in marketing or are eager to learn. You don't start from zero: LanguageTool
+    has a few hundred followers on twitter and more than 10,000 visits to its homepage per day. <!--You should be able to
+    regularly spend at least two hours per week on it.--> Would you like to take this job? Then please contact
+    <a href="mailto:daniel.naber@languagetool-removeme-.org"
+       onmouseover="this.href=this.href.replace(/-removeme-/,'');">LanguageTool maintainer Daniel Naber</a>
+    with a short introduction of yourself.
     </p>
     
 </div>
