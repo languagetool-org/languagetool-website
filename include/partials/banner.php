0f4301ec7b11c2a08ff0fc6ef2e15a5c07e8a217
Daniel Naber <naber@danielnaber.de>
2016-01-18 17:26:35 +0100

    replace the marketing job with a Community Manager job

--- include/partials/banner.php
+++ include/partials/banner.php
@@ -1,4 +1,4 @@
 <div style="background:#fee68a;padding:8px;color:black;text-align: center">
     <!--<a href="/job/growth_hacker.php">Help us to grow LanguageTool</a>-->
-    <a href="/job/marketing.php">Help us to market LanguageTool</a>
+    <a href="/job/community_manager.php">Become a Community Manager for LanguageTool</a>
 </div>
---
+++ www/job/community_manager.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang=en>
+<head>
+    <?php
+    $page = "other";
+    $title = "Job: LanguageTool Community Manager";
+    ?>
+    <?php include("../../include/header.php"); ?>
+</head>
+<body>
+<?php include("../../include/partials/nav.php"); ?>
+
+<div id="textContent">
+
+    <h2>Job: Community Manager</h2>
+    
+    <p>The LanguageTool project is looking for a volunteer who supports the project as a Community Manager.</p>
+
+    <p><strong>The community manager's task is to help the project have: More, happier, engaged users.</strong></p>
+    
+    <p>We've taken that definition from <a href="http://community.redhat.com/blog/2015/10/what-is-a-community-managers-goal/">Dave Neary</a>
+    who also wrote <a href="http://community.redhat.com/blog/2015/11/tales-of-a-first-time-community-manager/">another great article</a>
+    about community management.</p>
+
+    <p>However, you are free to decide what to do: you could blog, improve our website, or support users on the forum.
+    As LanguageTool is completely free we have no revenue and can't pay you money but you get the chance to be part of 
+    the team that develops the world's most powerful Open Source style and grammar checker. Also, Community Manager
+    is an established job now and if you want a career in community management, this is the perfect preparation.</p>
+    
+    <p>You won't start from zero: LanguageTool has more than 10,000 visits to its homepage per day. <!--You should be able to
+    regularly spend at least two hours per week on it.--> Would you like to take this job? Then please contact
+    <a href="mailto:daniel.naber@languagetool-removeme-.org"
+       onmouseover="this.href=this.href.replace(/-removeme-/,'');">LanguageTool maintainer Daniel Naber</a>
+    with a short introduction of yourself.
+    </p>
+    
+</div>
+
+<?php include("../../include/footer.php"); ?>
+
+</body>
+</html>
