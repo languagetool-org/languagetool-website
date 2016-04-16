30cc48f4714ab20e3c15c298e82e5a28f734b113
Daniel Naber <naber@danielnaber.de>
2016-04-16 12:07:37 +0200

    remove the "job" posting again, doesn't work...

--- include/partials/banner.php
+++ include/partials/banner.php
@@ -1,3 +1,5 @@
+<!--
 <div style="background:#fee68a;padding:8px;color:black;text-align: center">
     <a href="/job/machine-learning-developer.php">We're looking for a machine learning contributor</a>
 </div>
+-->
