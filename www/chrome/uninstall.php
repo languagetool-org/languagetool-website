1bfa33e1ff7d93d60a58b94af2d519e72d342884
Daniel Naber <naber@danielnaber.de>
2016-06-06 08:51:00 +0200

    allow voice feedback, maybe we get reasons why users uninstall the extension this way

--- www/chrome/uninstall.php
+++ www/chrome/uninstall.php
@@ -23,9 +23,10 @@
       document.write("<a href='mail" + "to:" + firstPart + "@" + lastPart + "'>email the developer<" + "/a>");
     // -->
     </script>
-    if there was a problem. We'd like to fix it.
-    </p>
-    
+    if there was a problem. We'd like to fix it. Or tell us your feedback here:</p>
+
+    <iframe src="https://www.speakpipe.com/widget/inline/am45pg807w9he467icv1xj45wq4i0lfp" frameborder="0" width="100%" height="180px"></iframe>
+
     <p>You can always use the latest version of LanguageTool without installing
     anything on <a href="/">our homepage</a>. To stay up to date on news about
     LanguageTool, <a href="/contact/newsletter.php">subscribe to our newsletter</a>.</p>
