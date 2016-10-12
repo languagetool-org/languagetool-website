239767b7032a658d8d11be60255f8329ca7385af
Daniel Naber <naber@danielnaber.de>
2016-10-12 14:05:34 +0200

    link our master thesis job

--- include/partials/banner.php
+++ include/partials/banner.php
@@ -1,5 +1,3 @@
-<!--
 <div style="background:#fee68a;padding:8px;color:black;text-align: center">
-    <a href="/job/machine-learning-developer.php">We're looking for a machine learning contributor</a>
+    <a href="/job/master-thesis.php">Master thesis job: development of a machine learning approach to detect grammar errors</a>
 </div>
--->
