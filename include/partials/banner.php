d41e75f2b85d09f587a41b0b112ca38d83bb9f12
Daniel Naber <naber@danielnaber.de>
2016-11-04 17:30:37 +0100

    comment out for now, looks like we've found someone

--- include/partials/banner.php
+++ include/partials/banner.php
@@ -1,3 +1,5 @@
+<!--
 <div style="background:#fee68a;padding:8px;color:black;text-align: center">
     <a href="/job/master-thesis.php">Master thesis job: development of a machine learning approach to detect grammar errors</a>
 </div>
+-->
