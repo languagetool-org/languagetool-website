fbfbcc04fd15b750d69bdebd435802e6de25c751
Daniel Naber <naber@danielnaber.de>
2016-12-26 21:09:55 +0100

    add thank you page

---
+++ www/donate/thankyou.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang=en>
+<head>
+    <?php
+    $page = "other";
+    $title = "Thank you for your donation to LanguageTool";
+    ?>
+    <?php include("../../include/header.php"); ?>
+</head>
+<body>
+<?php include("../../include/partials/nav.php"); ?>
+
+<div id="textContent">
+
+    <h2>Thank you</h2>
+
+    <p>Thanks for your donation to LanguageTool. If you want to be kept up-to-date about
+    major changes and new releases of LanguageTool, please subscribe to
+    our newsletter:</p>
+    
+    <?php include("../../include/newsletter.php") ?>
+
+</div>
+
+<?php include("../../include/footer.php"); ?>
+
+</body>
+</html>
