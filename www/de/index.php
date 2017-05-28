f16cd4170d9c6c47bb5357544b7c628ac59c1083
Daniel Naber <naber@danielnaber.de>
2017-05-28 15:13:56 +0200

    [de] extend this page

--- www/de/index.php
+++ www/de/index.php
@@ -134,17 +134,36 @@
                 <li>My <span class="errorMarker" title="'chef' bedeutet 'Chefkoch'/'Koch'. Meinten Sie 'chief' oder 'boss'?">chef</span> told me I'm not allowed to take vacation days.</li>
             </ul>
         </li>
+        <li>Datumsprüfung
+            <ul>
+                <li>Ich habe am <span class="errorMarker" title="Der Februar hat nur 28 Tage (bzw. 29 in Schaltjahren)">30. Februar</span> Geburtstag.</li>
+            </ul>
+        </li>
         <li>u.v.m.</li>
     </ul>
 
 
-    <h2 id="liboinstall">LanguageTool in LibreOffice/OpenOffice nutzen</h2>
+    <h2>LanguageTool benutzen</h2>
+    
+    <h3 id="browserinstall">LanguageTool im Browser</h3>
+    
+    <p>Firefox- und Chrome-User können LanguageTool als Erweiterung direkt im Browser installieren. So können Sie
+    einfach und schnell Texte prüfen, die Sie im Browser tippen, z.&nbsp;B. in Ihrem Webmailer:</p>
+    
+    <ul>
+        <li><a href="https://addons.mozilla.org/firefox/addon/languagetool?src=external-lt-homepage">LanguageTool im Verzeichnis der Firefox-Add-ons</a></li>
+        <li><a href="https://chrome.google.com/webstore/detail/languagetool/oldceeleldhonbafppcapldpdifcinji">LanguageTool im Chrome Web Store</a></li>
+    </ul>
+    
+    
+    <h3 id="liboinstall">LanguageTool in LibreOffice/OpenOffice</h3>
 
-    <p>LanguageTool steht als Extension (Erweiterung) für die <a href="http://de.wikipedia.org/wiki/Freie_Software" target="_blank">freien</a> Office-Pakete <a href="http://de.libreoffice.org/" target="_blank">LibreOffice</a> und <a href="http://de.openoffice.org/" target="_blank">Apache OpenOffice</a> zur Verfügung. Um LanguageTool in dem Office-Paket zu nutzen, folgen Sie einfach folgenden Schritten:</p>
+    <p>LanguageTool steht als Extension (Erweiterung) für die <a href="http://de.wikipedia.org/wiki/Freie_Software" target="_blank">freien</a> Office-Pakete <a href="http://de.libreoffice.org/" target="_blank">LibreOffice</a> und <a href="http://de.openoffice.org/" target="_blank">Apache OpenOffice</a> zur Verfügung. Um LanguageTool in dem Office-Paket zu nutzen, folgen Sie einfach diesen Schritten:</p>
 
     <ol>
         <li>Falls Java nicht auf Ihrem Computer installiert ist, laden Sie es sich <a href="http://www.java.com/de/download/manual.jsp" target="_blank">hier kostenlos herunter</a> und installieren Sie es. Ubuntu-Nutzer installieren bitte das Paket <a href="apt:libreoffice-java-common">libreoffice-java-common</a>.</li>
-        <li>Laden Sie sich die aktuelle Version von LanguageTool herunter. Der Downloadbutton befindet sich oben auf dieser Seite.</li>
+        <li>Laden Sie sich die aktuelle Version von LanguageTool herunter. Der Downloadbutton befindet sich oben auf dieser Seite mit
+            der Bezeichnung "Für LibreOffice und OpenOffice".</li>
         <li>Öffnen Sie LibreOffice bzw. OpenOffice.org und wählen Sie im Menü <i>Extras</i> den Punkt <i>Extension Manager</i>.</li>
         <li>Klicken Sie nun auf <i>Hinzufügen&hellip;</i>, wählen Sie die in Schritt 2 heruntergeladene Datei aus und klicken Sie auf <i>Öffnen</i>.</li>
         <li>Starten Sie nach der Installation LibreOffice bzw. OpenOffice.org neu.</li>
@@ -153,37 +172,81 @@
     Sofern die automatische Rechtschreibprüfung aktiviert ist, werden die von LanguageTool gefundenen Probleme im Text blau markiert. Die Einstellungen von LanguageTool können Sie über <i>Extras &rarr; LanguageTool &rarr; Konfiguration&hellip;</i> ändern.
 
 
-    <h2 id="standalone">LanguageTool als eigenständiges Programm benutzen</h2>
+    <h3 id="standalone">LanguageTool als eigenständiges Programm</h3>
+    
+    <p>Wer die oben beschriebenen Integrationen nicht nutzen kann oder will, kann es auch als offline-fähige Version
+    herunterladen:</p>
 
     <ol>
-        <li>Entpacken Sie die heruntergeladene zip-Datei (z.&nbsp;B. durch Rechtsklick &rarr; Entpacken &rarr; Hier o.&nbsp;ä.).</li>
+        <li>Entpacken Sie die heruntergeladene ZIP-Datei (z.&nbsp;B. durch Rechtsklick &rarr; Entpacken &rarr; Hier o.&nbsp;ä.).</li>
         <li>Führen Sie die Datei <tt>languagetool.jar</tt> mit Java aus (i.&nbsp;d.&nbsp;R. durch Doppelklick möglich).</li>
+        <li>In das LanguageTool-Fenster können Sie dann per Kopieren und Einfügen Texte zur Prüfung eingeben.</li>
     </ol>
 
-    <p>
-        In <a target="_blank" href="http://www.freiesmagazin.de/mobil/freiesMagazin-2012-08-bilder.html#12_08_languagetool1">freiesMagazin 08/2012</a> ist ein ausführlicher Artikel, in dem die Verwendungsmöglichkeiten und die Funktionsweise von LanguageTool beschrieben werden, erschienen.
-    </p>
+    <h3>Sonstige</h3>
+    
+    <p>Unsere Community hat weitere Add-ons entwickelt, u.&nbsp;a. für Eclipse, Sublime und MS Word. In unserem Wiki finden Sie
+    <a href="http://wiki.languagetool.org/software-that-supports-languagetool-as-a-plug-in-or-add-on">eine vollständige Liste</a>.</p>
 
 
-    <h2>Mitmachen</h2>
+    <h2>Neue Versionen</h2>
     
-    <p>Als Open-Source-Projekt ist LanguageTool auf die Mitarbeit von Freiwilligen angewiesen. In unserem
-    Wiki findet sich eine Beschreibung, <a href="http://wiki.languagetool.org/development-overview">wie man eigene Regeln
-    erstellt</a>, um in Zukunft noch mehr Fehler in Texten erkennen zu können.
-    Auch für Entwickler und Übersetzer gibt es bei LanguageTool <a href="http://wiki.languagetool.org/make-languagetool-better">spannende
-    Aufgaben</a>. </p>
+    <p>Alle drei Monate veröffentlichen wir eine neue Version zum Download (<a href="http://wiki.languagetool.org/roadmap">hier die Planung</a>).
+    Wer LanguageTool hier auf der Homepage oder als Firefox/Chrome-Add-on benutzt, nutzt automatisch immer die ganz aktuelle Version,
+    die täglich aktualisiert wird.</p>
     
     
-    <h2>Varianten</h2>
+    <h2>Sprachvarianten</h2>
+    
+    <p>Neben <em>Deutsch (Deutschland)</em> kann auch <em>Deutsch (Österreich)</em> und <em>Deutsch (Schweiz)</em>
+    als Sprache gewählt werden. Die Stil- und Grammatikregeln sind für alle drei Varianten die gleichen, die
+    Prüfung der Rechtschreibung wird aber entsprechend angepasst. Für die Schweiz wird auch beachtet, dass dort "ss" statt "ß"
+    benutzt wird.</p>
 
     <p>LanguageTool unterstützt auch <a href="leichte-sprache/">eine Prüfung auf Leichte Sprache</a>. Außerdem
     werden <a href="../languages/">mehr als 20 weitere Sprachen</a> unterstützt, darunter auch <a href="../">Englisch</a>,
     <a href="../fr/">Französisch</a> und <a href="../es/">Spanisch</a>.</p>
 
 
+    <h2>Mitmachen</h2>
+
+    <p>Als Open-Source-Projekt ist LanguageTool auf die Mitarbeit von Freiwilligen angewiesen. In unserem
+        Wiki findet sich eine Beschreibung, <a href="http://wiki.languagetool.org/development-overview">wie man eigene Regeln
+            erstellt</a>, um in Zukunft noch mehr Fehler in Texten erkennen zu können.
+        Auch für Entwickler und Übersetzer gibt es bei LanguageTool <a href="http://wiki.languagetool.org/make-languagetool-better">spannende
+            Aufgaben</a>. </p>
+
+    <p>
+        In <a target="_blank" href="http://www.freiesmagazin.de/mobil/freiesMagazin-2012-08-bilder.html#12_08_languagetool1">freiesMagazin 08/2012</a> ist ein ausführlicher Artikel, in dem die Verwendungsmöglichkeiten und die Funktionsweise von LanguageTool beschrieben werden, erschienen.
+    </p>
+
+
     <h2>Kontakt</h2>
 
     <p>Fragen beantworten wir im <a href="https://forum.languagetool.org">Forum</a>, wo Sie auch gerne Einträge auf Deutsch hinterlassen können. Dort ist es auch möglich, Fehlalarme von LanguageTool zu melden oder neue Regeln vorzuschlagen.</p>
+
+    
+    <h2>Wie funktioniert's?</h2>
+
+    <p>LanguageTool prüft jeden Text gleichzeitig mit drei Verfahren:</p>
+    
+    <ul>
+        <li><b>Rechtschreibprüfung:</b> LanguageTool prüft jedes Wort auf seine Rechtschreibung und benutzt dazu
+            das gleiche Wörterbuch wie LibreOffice, allerdings um mehr als 10.000 Wörter erweitert.</li>
+        <li><b>Fehlermuster:</b> LanguageTool verfügt für Deutsch über etwa 2200 Fehlermuster, die bestimmte Probleme in Texten 
+            erkennen. Diese Muster beziehen sich auf eine Folge von bestimmten Wörtern und Wortarten. Beispielsweise 
+            wird "ja schneller, desto" als Fehler erkannt, weil eine Regel existiert, die die Folge "ja",
+            gesteigertes Adjektiv, "desto" als wahrscheinlichen Fehler erkennt und statt "ja" das Wort "je" vorschlägt.</li>
+        <li><b>Statistik:</b> Viele Wortpaare aus ähnlichen Wörtern werden daraufhin geprüft, ob nicht möglicherweise
+            das andere Wort gemeint sein könnte. Dazu wird geprüft, wie oft das Wort üblicherweise in welchem
+            Kontext wie häufig vorkommt. Kommt ein Wort im konkreten Kontext wesentlich häufiger vor als das andere,
+            wird es als korrekt angenommen und das weniger häufig vorkommende als falsch.
+            Als Kontext werden jeweils zwei Wörter vor und nach dem betroffenen Wort benutzt.
+            Ein Beispiel dafür ist das Wortpaar dich/doch. Beide sind jeweils für sich genommen korrekt geschrieben &ndash; erst
+            durch den Kontext ergibt sich, welches Wort korrekt ist.
+            (<a href="http://wiki.languagetool.org/finding-errors-using-big-data">Details</a>)
+        </li>
+    </ul>
 
 </div>
 
