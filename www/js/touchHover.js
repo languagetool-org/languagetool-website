b8037b4eeb41868adc411672aaf62bf8e90d8a18
Md.Al-Amin <amin.se.aiub@gmail.com>
2017-06-01 01:29:53 +0600

    JS File Updated for Tooltip Update
    Made it more specific and add validation for null title.

--- www/js/touchHover.js
+++ www/js/touchHover.js
@@ -1,63 +1,40 @@
-/**
- * touchHover.js
- *
- * Create tooltips on mouseover or on click (for supporting touch interfaces).
- * 
- * @author C. Scott Asbach
- */
 
 $(document).ready(function() {	
 	
-	/**
-	 * store the value of and then remove the title attributes from the
-	 * abbreviations (thus removing the default tooltip functionality of
-         * the abbreviations)
-	 */
-	$('span').each(function(){		
+	$('.errorMarker').each(function(){		
 		
-		$(this).data('title',$(this).attr('title'));
+		$(this).data('title', $(this).attr('title'));
 		$(this).removeAttr('title');
 	
 	});
 
-	/**
-	 * when abbreviations are mouseover-ed show a tooltip with the data from the title attribute
-	 */	
-	$('span').mouseover(function() {		
+	$('.errorMarker').mouseover(function() {		
 		
-		// first remove all existing abbreviation tooltips
-		$('span').next('.tooltip').remove();
+		$('errorMarker').next('.tooltip').remove();
 		
 		// create the tooltip
-		$(this).after('<span class="tooltip">' + $(this).data('title') + '</span>');
+		if($(this).data('title') != ""){
+			$(this).after('<span class="tooltip">' + $(this).data('title') + '</span>');
+		}
 		
-		// position the tooltip 4 pixels above and 4 pixels to the left of the abbreviation
 		var left = $(this).position().left + $(this).width() + 4;
-		var top = $(this).position().top + 18;
+		var top = $(this).position().top +18;
 		$(this).next().css('left',left);
 		$(this).next().css('top',top);				
 		
 	});
 	
-	/**
-	 * when abbreviations are clicked trigger their mouseover event then fade the tooltip
-	 * (this is friendly to touch interfaces)
-	 */
-	$('span').click(function(){
+	$('.errorMarker').click(function(){
 		
 		$(this).mouseover();
 		
-		// after a slight 2 second fade, fade out the tooltip for 1 second
-		$(this).next().animate({opacity: 0.9},{duration: 4000, complete: function(){
+		$(this).next().animate({opacity: 0.9},{duration: 2000, complete: function(){
 			$(this).fadeOut(1000);
 		}});
 		
 	});
 	
-	/**
-	 * Remove the tooltip on abbreviation mouseout
-	 */
-	$('span').mouseout(function(){
+	$('.errorMarker').mouseout(function(){
 			
 		$(this).next('.tooltip').remove();				
 
