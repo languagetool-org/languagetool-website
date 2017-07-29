d4b2c76a3f4128c7a73cc6ecdf3307db6930ec9d
Daniel Naber <naber@danielnaber.de>
2017-07-29 22:06:44 +0200

    example of how to send via API instead of SMTP (SMTP seems to hang currently)

--- www/human-proofreading/submit.php
+++ www/human-proofreading/submit.php
@@ -5,29 +5,29 @@
 //   curl -X POST --data "data=ein testöäüßÖÄÜ&email=foo@domain.org&notes=xyz" http://languagetool.localhost/human-proofreading/submit.php
 
 error_reporting(E_ALL);
+require '../../mailjet-apiv3-php-no-composer/vendor/autoload.php';
+use \Mailjet\Resources;
 
-require '../../include/PHPMailerAutoload.php';
-$email = new PHPMailer();
 $timestamp = time();
-
-$email->isSMTP();
-$email->Host = 'in-v3.mailjet.com';
-$email->SMTPAuth = true;
-$email->Username = '28676616edd03dfa5d2524c121f61167';
-$email->Password = trim(file_get_contents("/home/languagetool/.mailjet.password"));
-$email->SMTPSecure = 'tls';
-$email->Port = 587;
-
-$email->From      = 'dont-reply@languagetool.org';
-$email->FromName  = 'PHP';
-$email->Subject   = 'Request for proofreading';
-$email->Body      = "See attachment, to be proofread for ".$_POST['email'].", timestamp: $timestamp\nNotes:\n".$_POST['notes'];
-$email->AddAddress('ltproofreading@gmail.com');
-
-$filename = "/tmp/lt-proofreading-".$timestamp.".txt";
-file_put_contents($filename , $_POST['data']);
-$email->AddAttachment($filename, 'request.txt');
-
-print $email->Send();
-
+$mj = new \Mailjet\Client('28676616edd03dfa5d2524c121f61167', trim(file_get_contents("/home/languagetool/.mailjet.password")));
+$body = [
+    'FromEmail' => "dont-reply@languagetool.org",
+    'FromName' => "PHP",
+    'Subject' => "Request for proofreading",
+    'Text-part' => "See attachment, to be proofread for ".$_POST['email'].", timestamp: $timestamp\nNotes:\n".$_POST['notes'],
+    'Recipients' => [
+        [
+            'Email' => "ltproofreading@gmail.com"
+        ]
+    ],
+    "Attachments" => [
+        [
+            "Content-type" => "text/plain",
+            "Filename" => "attachment.txt",
+            "content" => base64_encode($_POST['data'])
+        ]
+    ]
+];
+$response = $mj->post(Resources::$Email, ['body' => $body]);
+print $response->success();
 ?>
