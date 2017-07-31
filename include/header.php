2f5ea51f0185520dd236ccb0f31d2cb190a9498c
Christopher Blum <christopher.blum@spellboy.com>
2017-07-31 19:17:39 +0200

    Increase version number of css file

--- include/header.php
+++ include/header.php
@@ -21,7 +21,7 @@
   $assets = array(
     'links' => array(
       array(
-        'src' => getRoot() . '/css/style.css?v14',
+        'src' => getRoot() . '/css/style.css?v15',
         'rel' => "stylesheet",
         'type' => "text/css",
         'media' => 'screen'
