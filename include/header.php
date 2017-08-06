26db362cdeda4af801dbf3b122270f9e31ce66b1
Christopher Blum <christopher.blum@spellboy.com>
2017-08-06 16:03:38 +0200

    Increase version

--- include/header.php
+++ include/header.php
@@ -21,7 +21,7 @@
   $assets = array(
     'links' => array(
       array(
-        'src' => getRoot() . '/css/style.css?v16',
+        'src' => getRoot() . '/css/style.css?v17',
         'rel' => "stylesheet",
         'type' => "text/css",
         'media' => 'screen'
