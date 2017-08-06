6b0b178d2222a6741c25cb050d86ad6aff723de1
Christopher Blum <christopher.blum@spellboy.com>
2017-08-06 15:38:47 +0200

    Check if var is set

--- include/footer.php
+++ include/footer.php
@@ -23,7 +23,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
   var u=(("https:" == document.location.protocol) ? "https" : "http") + "://openthesaurus.stats.mysnip-hosting.de/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', <?php echo $isProofreadingTest ? "13" : "2"; ?>]);
+  _paq.push(['setSiteId', <?php echo (isset($isProofreadingTest) && $isProofreadingTest) ? "13" : "2"; ?>]);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
   g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
