048a9379ce067b88924395151e7dbb484ffb3c7d
Daniel Naber <naber@danielnaber.de>
2017-10-05 09:12:54 +0200

    new survey

--- www/online-check/tiny_mce/plugins/atd-tinymce/src/editor_plugin2.js
+++ www/online-check/tiny_mce/plugins/atd-tinymce/src/editor_plugin2.js
@@ -182,14 +182,14 @@
          
          editor.onPaste.add(function(editor, ev) {
              t._trackEvent('PasteText');
-             /*if (document.cookie.indexOf("addonSurveyShown=true") === -1) {
+             if (document.cookie.indexOf("addonSurveyShown=true") === -1) {
                  t._trackEvent('ShowAddonSurvey');
                  document.cookie = "addonSurveyShown=true;max-age=2628000";
-                 var surveyText = "Would you be interested in a LanguageTool integration in Microsoft Word?<br>If so, " +
-                     "<a target='_blank' href='https://www.surveymonkey.de/r/3BRKPPZ'>" +
-                     "please answer our 1-minute survey - just 2 questions</a>!";
+                 var surveyText = "Please help us improve LanguageTool by answering our " +
+                     "<a target='_blank' href='https://www.surveymonkey.de/r/LSPH6XY'>1-minute survey - 1 question only!</a>";
                  $('#feedbackErrorMessage').html("<div id='survey'>" + surveyText + "</div>");
-             }*/
+             }
+             /*
              var marketingText = "NEU: Unter <a href='https://languagetoolplus.com/'>languagetoolplus.com</a> bieten wir eine Premium-Version an, die noch mehr Fehler erkennt.";
              var randThreshold = 0.2;
              var langCode = $('#lang').val();
@@ -206,6 +206,7 @@
                      $('#feedbackErrorMessage').html("<div id='survey'>" + marketingText + "</div>");
                  }
              }
+             */
          });
 
          // hack to make both right and left mouse button work on errors in both Firefox and Chrome: 
