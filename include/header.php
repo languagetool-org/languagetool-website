68e01cd34ddd0e83ca45fb911149850208860926
Daniel Naber <naber@danielnaber.de>
2017-12-05 17:50:53 +0100

    force CSS reload

--- include/header.php
+++ include/header.php
@@ -21,7 +21,7 @@
   $assets = array(
     'links' => array(
       array(
-        'src' => getRoot() . '/css/style.css?v24',
+        'src' => getRoot() . '/css/style.css?v25',
         'rel' => "stylesheet",
         'type' => "text/css",
         'media' => 'screen'
