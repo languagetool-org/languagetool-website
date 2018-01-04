e663561625ec9eb18ab2ae5b7010b32d69f8fd99
Daniel Naber <naber@danielnaber.de>
2018-01-04 12:14:52 +0100

    [de] activate contact form

--- include/page_start.php
+++ include/page_start.php
@@ -12,20 +12,26 @@
               <p class="small"><a href="../contribute/"><?= $contributeLink ?></a></p>
           <?php } ?>
 
-          <?php if (strpos($_SERVER['HTTP_USER_AGENT'], "Chrome/") !== false && strpos($_SERVER['HTTP_USER_AGENT'], "Android") === false) { ?>
+          <?php if (rand(0, 1) > 0.25) { ?>
               <div id="addToBrowser">
-                  <a onclick="return installChromeExtension('calltoaction')" 
-                     href="https://chrome.google.com/webstore/detail/languagetool/oldceeleldhonbafppcapldpdifcinji"
-                  >
-                <?= isset($addToChrome) ? $addToChrome : 'Add to Chrome <span>free, no sign up required</span>' ?>
+                  <a href="#" onclick="contactForm();return false;">LanguageTool in Ihrer Firma nutzen?<span>&nbsp;&nbsp;Kontaktieren Sie uns</span></a>
               </div>
-          <?php 
-          } else if (strpos($_SERVER['HTTP_USER_AGENT'], "Firefox/") !== false && strpos($_SERVER['HTTP_USER_AGENT'], "Android") === false) { ?>
-              <div id="addToBrowser">
-                  <a target="_blank" href="https://addons.mozilla.org/firefox/addon/languagetool/?src=external-lt-homepage">
-                <?= isset($addToFirefox) ? $addToFirefox : 'Add to Firefox <span>free, no sign up required</span>' ?>
-              </div>
-          <?php } ?>
+          <?php } else { ?>
+              <?php if (strpos($_SERVER['HTTP_USER_AGENT'], "Chrome/") !== false && strpos($_SERVER['HTTP_USER_AGENT'], "Android") === false) { ?>
+                  <div id="addToBrowser">
+                      <a onclick="return installChromeExtension('calltoaction')"
+                         href="https://chrome.google.com/webstore/detail/languagetool/oldceeleldhonbafppcapldpdifcinji"
+                      >
+                          <?= isset($addToChrome) ? $addToChrome : 'Add to Chrome <span>free, no sign up required</span>' ?>
+                  </div>
+                  <?php
+              } else if (strpos($_SERVER['HTTP_USER_AGENT'], "Firefox/") !== false && strpos($_SERVER['HTTP_USER_AGENT'], "Android") === false) { ?>
+                  <div id="addToBrowser">
+                      <a target="_blank" href="https://addons.mozilla.org/firefox/addon/languagetool/?src=external-lt-homepage">
+                          <?= isset($addToFirefox) ? $addToFirefox : 'Add to Firefox <span>free, no sign up required</span>' ?>
+                  </div>
+              <?php } ?>
+          <?php } ?>           
 
       </div>
         <div id="editor">
