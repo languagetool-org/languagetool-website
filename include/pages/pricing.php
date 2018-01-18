9ca7b53c13d8997c5a487e727b485102f9a8b886
Daniel Naber <naber@danielnaber.de>
2018-01-18 11:56:18 +0100

    [ru] add section for Russian translation (not translated yet)

--- include/pages/pricing.php
+++ include/pages/pricing.php
@@ -232,6 +232,118 @@
                 <td></td>
             </tr>
 
+        <?php } if ($_SERVER['PHP_SELF'] == "/ru/index.php") { ?>
+
+            <tr>
+                <td></td>
+                <th>Free</th>
+                <th>Premium</th>
+                <th>Enterprise</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td>languagetool.org</td>
+                <td><a href="https://languagetoolplus.com/">languagetoolplus.com</a></td>
+                <td><a href="https://languagetoolplus.com/">languagetoolplus.com</a></td>
+            </tr>
+            <tr>
+                <td>Maximum characters per check</td>
+                <td>20,000</td>
+                <td>40,000</td>
+                <td>40,000+</td>
+            </tr>
+            <tr>
+                <td>Basic error detection for 20+ languages</td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td>Personal dictionary</td>
+                <td><?=$n?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td>More than 100 additional English errors detected</td>
+                <td><?=$n?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td>More than 100 additional German errors detected</td>
+                <td><?=$n?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td>Add-on for Firefox &amp; Chrome</td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td>Add-on for Google Docs</td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td>Add-on for MS Word (<a href="https://languagetoolplus.com/#msword">details</a>)</td>
+                <td><?=$n?></td>
+                <td><?=$y?></td>
+                <td><?=$y?></td>
+            </tr>
+            <tr>
+                <td style="vertical-align:top">Price</td>
+                <td style="vertical-align:top">free</td>
+                <td style="vertical-align:top">
+                    <a href='#' data-fsc-action="Add,Checkout" data-fsc-item-path-value="languagetool-plus-premium-monthly-subscription">
+                        <div class="buyButton">
+                            <div style="margin-top: 15px; text-align: center">
+                                <span data-fsc-item-path="languagetool-plus-premium-monthly-subscription" data-fsc-item-pricetotal></span>/month<br>
+                                <b>BUY NOW</b>
+                            </div>
+                        </div>
+                    </a>
+                    <br>
+                    <a href='#' data-fsc-action="Add,Checkout" data-fsc-item-path-value="languagetool-plus-premium-1-year-subscription">
+                        <div style="margin:0" class="buyButton">
+                            <span data-fsc-item-path="languagetool-plus-premium-1-year-subscription" data-fsc-item-pricetotal></span>/year<br>
+                            <!--<span class="smaller">i.e. less than <b>€7/month</b></span><br>-->
+                            <span class="smaller">you save 65%</span><br>
+                            <b>BUY NOW</b>
+                        </div>
+                    </a>
+                    <!--
+                    <br>or buy at <a href="https://languagetoolplus.com/#premium">languagetoolplus.com</a>
+                    -->
+                    <!--
+                    <a href="https://languagetoolplus.com/#premium">
+                        <div class="buyButton">
+                            buy at languagetoolplus.com
+                        </div>
+                    </a>
+                    -->
+                </td>
+                <td style="vertical-align:top">
+                    <script>
+                        var mt = "mail" + "to";
+                        var fp = "support";
+                        var domain = "languagetoolplus";
+                        var subject = "?subject=contact request for business premium (via lt.org)";
+                        var buttonText = "On Request";
+                        document.write("<a class='plan-button' href='" + mt + ":" + fp + "@" + domain + ".com" + subject + "'>" + buttonText + "</a>");
+                    </script>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td style="background-color: white">all prices incl. VAT</td>
+                <td></td>
+            </tr>
+            
         <?php } ?>
         
     </table>
