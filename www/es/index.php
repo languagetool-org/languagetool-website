ff9ed792afecb2ae2286fc5d65a13e70a9fd5bc0
David Méndez <hello@davidemdot.com>
2018-02-20 07:13:09 +0100

    [es] add translations to index.php

--- www/es/index.php
+++ www/es/index.php
@@ -13,56 +13,70 @@
     $title = "LanguageTool - corrector gramatical y de estilo";
 
     $checkLanguage = array(
-        'auto' => 'auto-detect',    
+        'auto' => 'detección automática',    
+        'de-DE' => 'alemán',
         'ast' => 'asturiano',
+        'be' => 'bielorruso',
         'br' => 'bretón',
+        'km' => 'camboyano',
+        'ca' => 'catalán',
         'zh' => 'chino',
         'da' => 'danés',
-        'de-DE' => 'alemán',
-        'en-US' => 'inglés',
+        'sk' => 'eslovaco',
+        'sl' => 'esloveno',
+        'es' => 'español',
         'eo' => 'esperanto',
         'fr' => 'francés',
         'gl' => 'gallego',
         'el' => 'griego',
+        'nl' => 'holandés',
+        'en-US' => 'inglés',
         //'is' => 'islandés',
         'it' => 'italiano',
         'ja' => 'japonés',
-        'ca' => 'catalán',
-        'km' => 'camboyano',
         //'lt' => 'lituano',
         //'ml' => 'malayalam',
-        'nl' => 'holandés',
+        'fa' => 'persa',
         'pl' => 'polaco',
         'pt' => 'portugués',
         'ro' => 'rumano',
         'ru' => 'ruso',
         'sv' => 'sueco',
-        'sk' => 'eslovaco',
-        'sl' => 'esloveno',
-        'es' => 'español',
         'tl' => 'tagalo',
-        'uk' => 'ucraniano',
-        'be' => 'bielorruso'
+        'ta' => 'tail',
+        'uk' => 'ucraniano'
     );
 
     $addYourTextHere = "Escriba un texto aquí"; // used for languages that have no demo text
     $checkSubmitButtonValue = "Verificar texto";
     $checkSubmitButtonTitle = "Verificar texto - también sirve presionar Ctrl+Return";
+    $demoTextLink = "Insertar texto de demostración";
     $toggleFullscreenMode = "Cambiar a pantalla completa";
 
     $introText1 = "<strong>LanguageTool</strong> es un software de Código Abierto de verificación gramatical, ortográfica y de estilo. Puedes utilizarlo en esta página o descargarlo gratuitamente.";
     $introText2 = "Además de español, soporta más de <a href='/languages/'>20 idiomas</a>.";
+    $addToChrome = "Añadir a Chrome <span>gratis y sin registro</span>";
+    $addToFirefox = "Añadir a Firefox <span>gratis y sin registro</span>";
 
+    $contributeLink = "¡Colabora!";
+    $forumHeadline = "Foro";
+    $developmentHeadline = "Desarrollo";
+    $compareEditionsText = "Comparar versiones";
+    $moreIntegrationsText = "Otros complementos";
     $downloadHeadline = "Descargar";
     $downloadRequiresJava = "Requiere Java {version}+";
     $downloadTitle = "Para <strong>LibreOffice</strong> / <strong>OpenOffice</strong>";
     $downloadTitleStandAlone = "Para <strong>tu escritorio</strong>";
     $downloadLabelFx = "Para <strong>Firefox</strong>";
     $downloadLabelChrome = "Para <strong>Chrome</strong>";
-    //$downloadLabelBrowserAddOn = "Browser Add-on";
+    $downloadLabelBrowserAddOn = "Extensión para el navegador";
+    $downloadLabelGoogleDocs = "Para <strong>Google Docs</strong>";
+    $downloadLabelAddOn = "Extensión";
     $checklistText = "¿Problemas? Mira nuestra <a href='/issues/'>página de ayuda</a>.";
     $otherDownloadsText = "Descarga versiones <a href='/download/'>antiguas</a> o <a href='/download/snapshots/?C=M;O=D'>diarias</a>.";
     $webstartText = "LanguageTool funciona con <a href='/webstart/web/LanguageTool.jnlp'>Java WebStart</a>.";
+    $firefoxLink = '<a href="/firefox/">Más información</a>';
+    $chromeLink = '<a href="/chrome/">Más información</a>';
     
     // ------------- TRANSLATIONS END HERE -------------
     ?>
