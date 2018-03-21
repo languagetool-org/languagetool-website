a7ffb5503c1fd0e81e769ff3bd228c410c4f082b
Daniel Naber <naber@danielnaber.de>
2018-03-21 12:44:50 +0100

    update for GDPR

--- www/privacy/index.php
+++ www/privacy/index.php
@@ -22,6 +22,14 @@
         über eine verschlüsselte Verbindung an unsere Server übertragen. Auf dieser Seite beschreiben wir,
         welche Daten wir speichern.</p>
 
+    <p><a href="/legal/">Unsere Kontaktinformationen finden Sie hier.</a></p>
+
+    <p>Hinweis: Sie können auch die "Stand-alone"-Version von <a href="/">unserer Homepage</a> herunterladen, die lokal läuft und
+        keinerlei Daten ins Internet überträgt.</p>
+
+    <p><b>Haben Sie uns eine Einwilligung zur Nutzung Ihrer Daten gegeben, z. B. durch Abonnieren des Newsletters,
+        können Sie diese jederzeit wie im Folgenden beschrieben widerrufen.</b></p>
+
     <ul>
         <li>Ihre zur Prüfung abgeschickten Texte werden nicht gespeichert, mit folgenden Ausnahmen:
             <ul>
@@ -35,7 +43,8 @@
                     Ursache zu finden und den Fehler zu beheben.</li>
             </ul>
             In all diesen Fällen speichern wir weder Ihre IP-Adresse, noch sonstige Informationen, 
-            die es uns erlauben würden, Sie zu identifizieren.
+            die es uns erlauben würden, Sie zu identifizieren. Da wird Ihnen Ihre Daten nicht mehr zuordnen können,
+            ist es auch nicht möglich, Ihre Daten gezielt wieder zu löschen.
         </li>
         <li>Um unser Angebot zu verbessern, speichern wir folgende Informationen:
             <ul>
@@ -45,15 +54,18 @@
                 <li>interne Fehler (wenn z.&nbsp;B. das Browser Add-on auf den zu prüfenden Text nicht zugreifen kann),</li>
                 <li>die Anzahl der Nutzungen des Browser-Add-ons (nur bei einer Deinstallation übertragen)</li>
             </ul>
+            Keine dieser Informationen kann genutzt werden, um Sie zu identifizieren.
         </li>
-        <li>Wenn Sie ein Kontaktformular ausfüllen, werden Ihre Daten per E-Mail an uns übertragen und gespeichert.</li>
+        <!--<li>Wenn Sie ein Kontaktformular ausfüllen, werden Ihre Daten per E-Mail an uns übertragen und gespeichert.</li>-->
         <li>In unseren Logdateien wird Ihre IP-Adresse in gekürzter Form (wie <tt>192.168.xxx.xxx</tt>) gespeichert,
             so dass sie nicht benutzt werden kann, um Sie zu identifizieren. Fehlermeldungen wie bei der Überschreitung des
-            Anfragelimits werden mit der vollständigen IP-Adresse gespeichert, damit wir gegen Missbrauch vorgehen können.</li>
+            Anfragelimits werden bis zu 48 Stunden lang mit der vollständigen IP-Adresse gespeichert, damit wir gegen Missbrauch
+            vorgehen können.</li>
         <li>Wenn Sie <a href="../contact/newsletter.php">unseren Newsletter</a> abonnieren, wird Ihre E-Mail-Adresse bei
-            unserem Newsletter-Anbieter (newsletter2go.com) gespeichert.</li>
-        <li>Sie können die "Stand-alone"-Version von <a href="/">unserer Homepage</a> herunterladen, die lokal läuft und
-            keinerlei Daten ins Internet überträgt.
+            unserem Newsletter-Anbieter (<a href="https://www.newsletter2go.com/">newsletter2go.com</a>, Newsletter2Go GmbH,
+            Köpenicker Straße 126, 10179 Berlin, <a href="https://www.newsletter2go.de/datenschutz/">Datenschutzerklärung</a>) gespeichert.
+            Ihre E-Mail-Adresse wird wieder gelöscht, wenn Sie sich über den in jedem Newsletter vorhandenen Abmelde-Link
+            abmelden. Sie können uns dazu auch per E-Mail kontaktieren (<a href="/legal/">unsere Kontaktdaten</a>). 
         </li>
         <li>
             <p>Die meisten Browser sind automatisch so eingestellt, dass Cookies akzeptiert werden. Auf Wunsch können
@@ -83,12 +95,35 @@
             </ul>
         </li>
         <li>
-            <p>Diese Website benutzt <a href="http://www.piwik.org">Piwik</a> für die Analyse der Zugriffszahlen. Auch hier wird Ihre
-                IP-Adresse nur in gekürzter Form gespeichert (z.&nbsp;B. <tt>192.168.xxx.xxx</tt>). Wenn Ihre Besuche trotzdem über
-                Piwik gar nicht erfasst werden sollen, können Sie hier Ihre Zustimmung verweigern:</p>
+            <p>Unserer Website verwendet Piwik/Matomo, gehostet bei <a href="http://www.mysnip-solutions.de/impressum.html">mysnip</a>
+                (MySnip Solutions - Thomas Seifert, Kreutzigerstr. 4a, 10247 Berlin). Dabei handelt es sich um einen sogenannten Webanalysedienst.
+                Piwik verwendet sog. “Cookies”, das sind Textdateien, die auf Ihrem Computer gespeichert werden und die unsererseits eine 
+                Analyse der Benutzung der Webseite ermöglichen. Zu diesem Zweck werden die durch den Cookie erzeugten Nutzungsinformationen
+                (einschließlich Ihrer gekürzten IP-Adresse) an unseren Server übertragen und zu Nutzungsanalysezwecken gespeichert, was
+                der Webseitenoptimierung unsererseits dient. Ihre IP-Adresse wird bei diesem Vorgang umgehend anonymisiert, so 
+                dass Sie als Nutzer für uns anonym bleiben. Sie können die Verwendung der Cookies durch eine entsprechende Einstellung 
+                Ihrer Browser Software verhindern, es kann jedoch sein, dass Sie in diesem Fall gegebenenfalls nicht sämtliche 
+                Funktionen dieser Website voll umfänglich nutzen können.</p>
+            <p>Wenn Sie mit der Speicherung und Auswertung dieser Daten aus Ihrem Besuch nicht einverstanden sind, dann können Sie der
+                Speicherung und Nutzung nachfolgend per Mausklick jederzeit widersprechen. In diesem Fall wird in Ihrem Browser ein sog.
+                Opt-Out-Cookie abgelegt, was zur Folge hat, dass Piwik keinerlei Sitzungsdaten erhebt. <b>Achtung:</b> Wenn Sie Ihre Cookies 
+                löschen, so hat dies zur Folge, dass auch das Opt-Out-Cookie gelöscht wird und ggf. von Ihnen erneut aktiviert werden muss.</p>
 
             <iframe frameborder="no" width="600px" height="250px" src="//openthesaurus.stats.mysnip-hosting.de/index.php?module=CoreAdminHome&amp;action=optOut"></iframe>
         </li>
+        <li>Bestellen Sie ein kostenpflichtiges Premium-Paket, wird die Bestellung über unseren Reseller <a href="https://fastspring.com">fastspring</a>
+            (Bright Market, LLC d/b/a FastSpring. 801 Garden St., Santa Barbara, CA 93101) abgewickelt.
+            Die Daten bei Fastspring unterliegen dem <a href="https://www.privacyshield.gov">EU-US Privacy Shield</a> und
+            fastspring hat sich zur Einhaltung der EU-Datenschutzverordnung ab dem 25. Mai 2018 verpflichtet.
+            Ihre Daten werden nach einer Abo-Kündigung aus steuerlichen Gründen bei fastspring aufbewahrt. Details
+            dazu finden Sie in der <a href="https://fastspring.com/privacy/">Datenschutzerklärung von fastspring</a>.
+            Ihre Kreditkartendaten - sofern Sie eine Kreditkarte zum Erwerb des Premium-Produkts benutzen - werden an fastspring
+            übertragen und sind LanguageTool nicht bekannt.
+        </li>
+        <li>Sie haben das Recht, auf Antrag unentgeltlich Auskunft zu erhalten über die personenbezogenen Daten, die über Sie
+            gespeichert wurden.</li>
+        <li>Wenn Sie der Ansicht sind, dass die Verarbeitung Ihrer personenbezogenen Daten rechtswidrig erfolgt, haben Sie das Recht,
+            sich bei einer Aufsichtsbehörde zu beschweren.</li>
     </ul>
 
     <p><a href="https://github.com/languagetool-org/languagetool-website/commits/master/www/privacy/index.php">Änderungshistorie dieser Seite</a></p>
@@ -99,7 +134,15 @@
     
     <p>LanguageTool is a proofreading software. It works by sending the text to be checked to our servers over an
     encrypted connection. This policy describes what kind of data we store.</p>
-    
+
+    <p><a href="/legal/">Here's how you can contact us.</a></p>
+
+    <p>Note: If you don't want any information to be transferred, download the stand-alone version from
+        <a href="/">our homepage</a>. It works locally without internet access and thus doesn't transfer any data.
+
+    <p><b>Have you given us permission to use your data, e.g. by subscribing to the newsletter?
+        You can revoke this permission at any time as described below.</b></p>
+
     <ul>
         <li>We don't store the text that you submit for style and grammar checking on languagetool.org, with the following exceptions:
             <ul>
@@ -111,7 +154,7 @@
                     we can reproduce and fix the error.</li>
             </ul>
             In any case, we don't store the data with your IP address or any other meta information
-            that would allow us to identify you.
+            that would allow us to identify you. Thus it's also not possible to delete your data.
         </li>
         <li>To improve our proofreading service, we keep a log of the following information:
             <ul>
@@ -120,17 +163,17 @@
                 <li>internal errors that occur (e.g. the <a href="../firefox">browser add-on</a> not being able to access the text to be checked),</li>
                 <li>the number of times you used the browser add-on (only transferred when you uninstall the add-on)</li>
             </ul>
-            This is an example of a log entry that LanguageTool generates on our server:<br/>
-            <tt>2014-02-24 12:10:24 Check done: 793 chars, en-US, https://languagetool.org/, handlers:1, 3 matches, 132ms, sent</tt>
+            None of this information can be used to identify you.
         </li>
-        <li>If you enter data in a contact form, this data will be transferred via email and it will be stored.</li>
+        <!--<li>If you enter data in a contact form, this data will be transferred via email and it will be stored.</li>-->
         <li>In our web server log files, your IP address is stored in an abbreviated form (like <tt>192.168.xxx.xxx</tt>)
             so it cannot be used to identify you. Error messages like for exceeding the query limit are stored with the
-            full IP so we can prevent abuse.</li>
+            full IP for up to 48 hours so we can prevent misuse of the service.</li>
         <li>If you subscribe to <a href="../contact/newsletter.php">our newsletter</a>, your email address will be stored
-            at our newsletter provider (newsletter2go.com) </li>
-        <li>If you don't want any information to be transferred, download the stand-alone version from 
-            <a href="/">our homepage</a>. It works locally without internet access and thus doesn't transfer any data.
+            at our newsletter provider (<a href="https://www.newsletter2go.com/">newsletter2go.com</a>, Newsletter2Go GmbH,
+            Köpenicker Straße 126, 10179 Berlin, Germany, <a href="https://www.newsletter2go.de/datenschutz/">Privacy Policy</a>).
+            Your email will be deleted when you unsubscribe using the unsubscribe link that is included in every
+            newsletter we send. You can also <a href="/legal/">contact us</a> to unsubscribe from the newsletter.
         </li>
         <li>
             <p>Most browsers are configured to accept cookies. However, you can change your 
@@ -162,6 +205,17 @@
 
             <iframe frameborder="no" width="600px" height="250px" src="//openthesaurus.stats.mysnip-hosting.de/index.php?module=CoreAdminHome&amp;action=optOut"></iframe>            
         </li>
+        <li>In case you order a premium subscription, your order will be processed by our reseller <a href="https://fastspring.com">fastspring</a>
+            (Bright Market, LLC d/b/a FastSpring. 801 Garden St., Santa Barbara, CA 93101).
+            The data at fastspring is protected under the <a href="https://www.privacyshield.gov">EU-US Privacy Shield</a>, additionally
+            fastspring has committed to follow all EU privacy laws starting 2018-05-28.
+            Your data will be kept at fastspring after you cancel a susbcription for tax reasons. See
+            <a href="https://fastspring.com/privacy/">their privacy policy</a> for details.
+            Your credit card information - if you use a credit card to purchase the premium product - is
+            transmitted to fastspring, LanguageTool does not know your credit card data.
+        </li>
+        <li>You have the right, upon request and free of charge, to receive information about your personal data we stored.</li>
+        <li>If you believe that the processing of your personal data is unlawful, you have the right to complain to a regulatory agency.</li>
     </ul>
     
     <p><a href="https://github.com/languagetool-org/languagetool-website/commits/master/www/privacy/index.php">Change log of this page</a></p>
